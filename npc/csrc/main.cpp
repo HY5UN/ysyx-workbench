0d881e3bc7dfc56cd4e6792ae584243b3b3e572c
tracer-ysyx <tracer@ysyx.org>
2025-12-29 03:03:34 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:03:34 up 14:37,  1 user,  load average: 0.00, 0.02, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include "Vtop.h"
+#include"verilated.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+int main(int argc, char** argv){
+    VerilatedContext* contextp =new VerilatedContext;
+    contextp->commandArgs(argc,argv);
+    
+    Vtop* top = new Vtop{contextp};
+
+    while(1){
+        int a=rand()&1;
+        int b=rand()&1;
+
+        top->a=a;
+        top->b=b;
+
+        top->eval();
+        printf("a=%d b=%d out=%d\n",a,b,top->f);
+        assert((a^b) == top->f);
+
+         break; // Uncomment to run only one iteration
+    }
+
+    delete top;
+    delete contextp;
+    return 0;
 }
+
