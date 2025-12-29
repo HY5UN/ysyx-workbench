a84956f35a0f875181ca572ff05dc8667ea2f3ce
tracer-ysyx <tracer@ysyx.org>
2025-12-29 21:35:46 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:35:46 up  8:33,  1 user,  load average: 0.07, 0.08, 0.14

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,8 +1,20 @@
 module top(
-  input sw0,
-  input sw1,
-  output ld0
+  input clk,
+  input rst,
+  output reg [15:0] led;
 );
-  assign ld0 = sw0 ^ sw1; 
 
+  reg [31:0] cnt;
+  always @(posedge clk) begin
+    if(rst) begin led<=16'h0001;count<=0; end
+    else begin
+      if(cnt==32'd50000000) begin
+        cnt<=0;
+        led<={led[14:0],led[15]};
+      end
+      else cnt<=cnt+1;
+
+  end
+  end
+  
 endmodule
