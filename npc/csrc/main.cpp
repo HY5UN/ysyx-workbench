ece03cae37c8481ab14facb032b7700fbdd0d648
tracer-ysyx <tracer@ysyx.org>
2025-12-29 18:00:02 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:00:02 up  4:49,  1 user,  load average: 0.40, 0.19, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,5 +1,6 @@
 #include "Vtop.h"
 #include"verilated.h"
+#include"verilated_fst_c.h"
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -8,10 +9,15 @@
 int main(int argc, char** argv){
     VerilatedContext* contextp =new VerilatedContext;
     contextp->commandArgs(argc,argv);
+    contextp->traceEverOn(true);
     
     Vtop* top = new Vtop{contextp};
 
-    while(1){
+    VerilatedFstC* tfp = new VerilatedFstC;
+    top->trace(tfp,99);
+    tfp->open("waveform.fst");
+
+    while(!contextp->gotFinish()){
         int a=rand()&1;
         int b=rand()&1;
 
@@ -19,14 +25,17 @@ int main(int argc, char** argv){
         top->b=b;
 
         top->eval();
+        tfp->dump(contextp->time());
         printf("a=%d b=%d out=%d\n",a,b,top->f);
         assert((a^b) == top->f);
 
-         break; // Uncomment to run only one iteration
+        if(contextp->time() >20) break;
+        contextp->timeInc(1);
     }
 
     delete top;
     delete contextp;
+    delete tfp;
     return 0;
 }
 
