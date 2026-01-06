917276cee7760a2661cf36e6b393816df4493db5
tracer-ysyx <tracer@ysyx.org>
2026-01-06 21:35:32 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:35:32 up  5:01,  1 user,  load average: 0.16, 0.15, 0.16

--- npc/playground/src/Mux21.scala
+++ npc/playground/src/Mux21.scala
@@ -4,19 +4,10 @@ import chisel3._
 import chisel3.util._   
 class Mux41 extends Module {
   val io = IO(new Bundle {
-    val in0 = Input(UInt(1.W))
-    val in1 = Input(UInt(1.W))
-    val in2 = Input(UInt(1.W))
-    val in3 = Input(UInt(1.W))
+    val in=Input(UInt(4.W))
     val sel = Input(UInt(2.W))
     val out = Output(UInt(1.W))
   })
 
-    io.out := MuxLookup(io.sel, 0.U, Array(
-        0.U -> io.in0,
-        1.U -> io.in1,
-        2.U -> io.in2,
-        3.U -> io.in3
-    ))
-
+  io.out := io.in(io.sel);
 }
