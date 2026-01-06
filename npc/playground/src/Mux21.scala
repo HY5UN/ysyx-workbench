4e2a184c43ca08d977deb12f9b1a1bb13eb6f641
tracer-ysyx <tracer@ysyx.org>
2026-01-06 20:34:34 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:34:34 up  4:00,  1 user,  load average: 0.11, 0.09, 0.08

---
+++ npc/playground/src/Mux21.scala
@@ -0,0 +1,13 @@
+
+import chisel3._
+
+class Mux21 extends Moule {
+  val io = IO(new Bundle {
+    val in0 = Input(UInt(1.W))
+    val in1 = Input(UInt(1.W))
+    val sel = Input(Bool())
+    val out = Output(UInt(1.W))
+  })
+
+  io.out := Mux(io.sel, io.in1, io.in0)
+}
