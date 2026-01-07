1d6d1476341ec008ea7107f4ad80b0162bf2be88
tracer-ysyx <tracer@ysyx.org>
2026-01-08 01:57:58 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:57:58 up 23:31,  1 user,  load average: 0.05, 0.13, 0.25

--- npc/playground/src/PS2KeyboardRx.scala
+++ npc/playground/src/PS2KeyboardRx.scala
@@ -3,19 +3,19 @@ import chisel3._
 import chisel3.util._
 
 class PS2KeyboardRx extends Module {
-  val io = IO(new Bundle {
+  val io      = IO(new Bundle {
     val ps2clk  = Input(Bool())
     val ps2data = Input(Bool())
 
     val data  = Output(UInt(8.W))
     val ready = Output(Bool())
   })
-	val dataReg = RegInit(0.U(8.W))
-	io.data := dataReg
-	val readyReg = RegInit(false.B)
-	io.ready := readyReg 
-	
-  val ps2ClkSync = RegInit(1.U(3.W))
+  val dataReg = RegInit(0.U(8.W))
+  io.data := dataReg
+  val readyReg = RegInit(false.B)
+  io.ready := readyReg
+
+  val ps2ClkSync = RegInit(7.U(3.W))
   ps2ClkSync := Cat(ps2ClkSync(1, 0), io.ps2clk)
 
   val ps2Clk = ps2ClkSync(2) & ~ps2ClkSync(1)
@@ -23,23 +23,29 @@ class PS2KeyboardRx extends Module {
   val bitCnt    = RegInit(0.U(4.W))
   val shiftReg  = RegInit(0.U(10.W))
   val receiving = RegInit(false.B)
+  val start     = RegInit(false.B)
 
   when(ps2Clk) {
-    when(bitCnt === 10.U) {
-      receiving := false.B
-      dataReg   := shiftReg(7, 0)
-      val oddOk  = shiftReg(8, 0).xorR
-      val stopOk = shiftReg(9) === 1.B
-      readyReg := oddOk & stopOk
-      bitCnt   := 0.U
-    }
+      readyReg  := false.B
+    
     when(!receiving & io.ps2data === 0.B) {
+      start := true.B
+
+    }.elsewhen(start) {
       receiving := true.B
       bitCnt    := 0.U
+      start     := false.B
     }.elsewhen(receiving) {
       bitCnt := bitCnt + 1.U
 
       shiftReg := Cat(io.ps2data, shiftReg(9, 1))
+    }.elsewhen(bitCnt === 10.U) {
+      receiving := false.B
+      dataReg   := shiftReg(7, 0)
+      val oddOk  = (shiftReg(7, 0).xorR ^ shiftReg(8)) === 1.B
+      val stopOk = shiftReg(9) === 1.B
+      readyReg := oddOk & stopOk
+      bitCnt   := 0.U
     }
   }
 }
