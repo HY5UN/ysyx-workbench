8c023b6f4e429c9fb23979cadaead92f944bae4c
tracer-ysyx <tracer@ysyx.org>
2026-01-08 02:20:38 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:20:38 up 23:54,  1 user,  load average: 0.78, 0.37, 0.29

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -17,45 +17,45 @@ class top extends Module {
   rx.io.ps2data := io.ps2data
 
   val keyDown  = RegInit(false.B)
-val lastCode = RegInit(0.U(8.W))
-val sawF0    = RegInit(false.B)
+  val lastCode = RegInit(0.U(8.W))
+  val sawF0    = RegInit(false.B)
 
-when(rx.io.ready) {
-  val b = rx.io.data
+  when(rx.io.ready) {
+    val b = rx.io.data
 
-  when(sawF0) {
-    // 这一帧是 break 的“键码帧”，表示释放
-    sawF0 := false.B
-    keyDown := false.B
-    lastCode := 0.U // 可选：清掉
-  }.elsewhen(b === "hF0".U) {
-    // break 前缀
-    sawF0 := true.B
-  }.otherwise {
-    // make code
-    lastCode := b
-    when(!keyDown) {
-      keyDown := true.B
-      // 这里如果要计数，也只在 !keyDown 时 +1
+    when(sawF0) {
+      // 这一帧是 break 的“键码帧”，表示释放
+      sawF0    := false.B
+      keyDown  := false.B
+      lastCode := 0.U // 可选：清掉
+    }.elsewhen(b === "hF0".U) {
+      // break 前缀
+      sawF0 := true.B
     }.otherwise {
-      // 按住产生重复码：忽略（不计数不更新也行）
-      // 如果你想更新 lastCode 也可以，但不建议
+      // make code
+      lastCode := b
+      when(!keyDown) {
+        keyDown := true.B
+        // 这里如果要计数，也只在 !keyDown 时 +1
+      }.otherwise {
+        // 按住产生重复码：忽略（不计数不更新也行）
+        // 如果你想更新 lastCode 也可以，但不建议
+      }
     }
   }
-}
 
-io.keydown := keyDown
+  io.keydown := keyDown
 
-when(keyDown) {
-  io.hex(0) := SevenSeg.encodeHex0toF(lastCode(3, 0), true.B)
-  io.hex(1) := SevenSeg.encodeHex0toF(lastCode(7, 4), true.B)
-}.otherwise {
-  io.hex(0) := SevenSeg.encodeHex0toF(0.U, false.B)
-  io.hex(1) := SevenSeg.encodeHex0toF(0.U, false.B)
-}
+  when(keyDown) {
+    io.hex(0) := SevenSeg.encodeHex0toF(lastCode(3, 0), true.B)
+    io.hex(1) := SevenSeg.encodeHex0toF(lastCode(7, 4), true.B)
+  }.otherwise {
+    io.hex(0) := SevenSeg.encodeHex0toF(0.U, false.B)
+    io.hex(1) := SevenSeg.encodeHex0toF(0.U, false.B)
+  }
   // temp
   val code = RegInit(0.U(8.W))
-   code := rx.io.data
+  code      := rx.io.data
   io.hex(2) := SevenSeg.encodeHex0toF(code(3, 0), true.B)
   io.hex(3) := SevenSeg.encodeHex0toF(code(7, 4), true.B)
   io.hex(4) := SevenSeg.encodeHex0toF(0.U, false.B)
