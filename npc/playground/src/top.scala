2de18b867af6eafdcd22681764b02809bbfdb76b
tracer-ysyx <tracer@ysyx.org>
2026-01-08 01:31:24 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:31:24 up 23:04,  1 user,  load average: 0.13, 0.36, 0.36

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -1,52 +1,41 @@
 package lab
 import chisel3._
-import chisel3.util._   
+import chisel3.util._
 
 class top extends Module {
   val io = IO(new Bundle {
-    val ps2clk= Input(Bool())
-    val ps2data= Input(Bool())
+    val ps2clk  = Input(Bool())
+    val ps2data = Input(Bool())
 
-    val hex= Output(Vec(6, UInt(7.W)))
-    //temp
+    val hex     = Output(Vec(6, UInt(7.W)))
+    // temp
     val keydown = Output(Bool())
   })
 
   val rx = Module(new PS2KeyboardRx)
-  rx.io.ps2clk := io.ps2clk
+  rx.io.ps2clk  := io.ps2clk
   rx.io.ps2data := io.ps2data
-  
-  
-  val keyDown  = RegInit(false.B)
-val sawF0    = RegInit(false.B)
-val lastCode = RegInit(0.U(8.W))
 
-when(rx.io.ready) {
-  when(sawF0) {
-    // 这是 break 的第二帧：释放的键码
-    sawF0 := false.B
-    keyDown := false.B
-  }.elsewhen(rx.io.data === "hF0".U) {
-    sawF0 := true.B
-  }.otherwise {
-    // make code
-    when(!keyDown) {
-      keyDown := true.B
-      lastCode := rx.io.data   // 只在首次按下更新显示
+  val keyDown  = RegInit(false.B)
+  val lastCode = RegInit(0.U(8.W))
+  when(rx.io.ready) {
+    lastCode := rx.io.data
+    when(rx.io.data === "hF0".U) {
+      keyDown := false.B
+    }.otherwise {
+      when(!keyDown) { keyDown := true.B }
     }
-    // keyDown=true 时来的重复码直接忽略
   }
-}
-io.keydown := keyDown
+  io.keydown := keyDown
 
   when(keyDown) {
-    io.hex(0) := SevenSeg.encodeHex0toF(lastCode(3,0), true.B)
-    io.hex(1) := SevenSeg.encodeHex0toF(lastCode(7,4), true.B)
+    io.hex(0) := SevenSeg.encodeHex0toF(lastCode(3, 0), true.B)
+    io.hex(1) := SevenSeg.encodeHex0toF(lastCode(7, 4), true.B)
   }.otherwise {
     io.hex(0) := SevenSeg.encodeHex0toF(0.U, false.B)
     io.hex(1) := SevenSeg.encodeHex0toF(0.U, false.B)
   }
-  //temp
+  // temp
   io.hex(2) := SevenSeg.encodeHex0toF(0.U, false.B)
   io.hex(3) := SevenSeg.encodeHex0toF(0.U, false.B)
   io.hex(4) := SevenSeg.encodeHex0toF(0.U, false.B)
