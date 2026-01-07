047469ce852c46151799eb30e99da8f674d55b29
tracer-ysyx <tracer@ysyx.org>
2026-01-08 02:01:53 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:01:53 up 23:35,  1 user,  load average: 0.28, 0.27, 0.28

--- npc/playground/src/PS2KeyboardRx.scala
+++ npc/playground/src/PS2KeyboardRx.scala
@@ -26,26 +26,34 @@ class PS2KeyboardRx extends Module {
   val start     = RegInit(false.B)
 
   when(ps2Clk) {
-      readyReg  := false.B
-    
-    when(!receiving & io.ps2data === 0.B) {
-      start := true.B
+  readyReg := false.B
 
-    }.elsewhen(start) {
-      receiving := true.B
-      bitCnt    := 0.U
-      start     := false.B
-    }.elsewhen(receiving) {
-      bitCnt := bitCnt + 1.U
+  when(!receiving && (io.ps2data === 0.B)) {
+    start := true.B
+  }.elsewhen(start) {
+    receiving := true.B
+    bitCnt := 0.U
+    start := false.B
+  }.elsewhen(receiving) {
+    // 收一位：data/parity/stop 共10位
+    shiftReg := Cat(io.ps2data, shiftReg(9, 1))
 
-      shiftReg := Cat(io.ps2data, shiftReg(9, 1))
-    }.elsewhen(bitCnt === 10.U) {
+    when(bitCnt === 9.U) { // 第10次采样完成
       receiving := false.B
-      dataReg   := shiftReg(7, 0)
-      val oddOk  = (shiftReg(7, 0).xorR ^ shiftReg(8)) === 1.B
-      val stopOk = shiftReg(9) === 1.B
-      readyReg := oddOk & stopOk
-      bitCnt   := 0.U
+      dataReg := shiftReg(7, 0) // 注意：这是“shift 之前”的值，如果要用 shift 后的，需要用 nextShift
+      val dataNext = Cat(io.ps2data, shiftReg(9,1))(7,0)
+      val parityNext = Cat(io.ps2data, shiftReg(9,1))(8)
+      val stopNext = Cat(io.ps2data, shiftReg(9,1))(9)
+
+      dataReg := dataNext
+      val oddOk  = (dataNext.xorR ^ parityNext) === 1.B
+      val stopOk = stopNext === 1.B
+      readyReg := oddOk && stopOk
+      bitCnt := 0.U
+    }.otherwise {
+      bitCnt := bitCnt + 1.U
     }
   }
+}
+
 }
