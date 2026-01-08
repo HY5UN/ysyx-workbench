0a745f8020da7bf19c04e7cf45d254fd3239c2bf
tracer-ysyx <tracer@ysyx.org>
2026-01-08 18:23:34 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:23:34 up 1 day,  4:51,  1 user,  load average: 0.03, 0.07, 0.03

--- npc/playground/src/PS2KeyboardRx.scala
+++ npc/playground/src/PS2KeyboardRx.scala
@@ -4,48 +4,60 @@ import chisel3.util._
 
 class PS2KeyboardRx extends Module {
   val io = IO(new Bundle {
-    val ps2clk  = Input(Bool())
-    val ps2data = Input(Bool())
-    val data    = Output(UInt(8.W))
-    val ready   = Output(Bool())
+
+    val ps2clk     = Input(Bool())
+    val ps2data    = Input(Bool())
+    val nextdata_n = Input(Bool())
+    val data       = Output(UInt(8.W))
+    val ready      = Output(Bool())
+    val overflow   = Output(Bool())
+
   })
 
-  // 同步+下降沿检测
-  val ps2ClkSync = RegInit(7.U(3.W))                 // 111
+  val readyReg    = RegInit(false.B)
+  val overflowReg = RegInit(false.B)
+
+  val ps2ClkSync = RegInit(7.U(3.W))
   ps2ClkSync := Cat(ps2ClkSync(1, 0), io.ps2clk.asUInt)
-  val sampling = ps2ClkSync(2) && !ps2ClkSync(1)     // falling edge
+  val sampling = ps2ClkSync(2) && !ps2ClkSync(1)
 
-  val count  = RegInit(0.U(4.W))     // 0..10
-  val bits   = RegInit(0.U(11.W))    // start + 8data + parity + stop
-  val dataR  = RegInit(0.U(8.W))
-  val readyR = RegInit(false.B)
+  val fifo   = Reg(Vec(8, UInt(8.W)))
+  val wPtr   = RegInit(0.U(3.W))
+  val rPtr   = RegInit(0.U(3.W))
+  val count  = RegInit(0.U(4.W))
+  val buffer = RegInit(0.U(10.W))
 
-  io.data  := dataR
-  io.ready := readyR
-
-  // ready 只打一拍
-  readyR := false.B
-
-  when(sampling) {
-    val nextBits = bits.bitSet(count, io.ps2data) // ✅ 关键：用 nextBits
-    bits := nextBits
-
-    when(count === 10.U) {
-      val startOk = nextBits(0) === 0.U
-      val data    = nextBits(8, 1)       // LSB-first -> 直接就是扫描码
-      val parity  = nextBits(9)
-      val stopOk  = nextBits(10) === 1.U
-
-      // odd parity：data XOR parity = 1
-      val oddOk   = (data.xorR ^ parity) === 1.B
-
-      when(startOk && stopOk && oddOk) {
-        dataR  := data
-        readyR := true.B
-      }
-      count := 0.U
-    }.otherwise {
-      count := count + 1.U
+  when(!io.nextdata_n && readyReg) {
+    rPtr := rPtr + 1.U
+    when(rPtr === wPtr) {
+      readyReg := false.B
     }
   }
+
+  when(sampling) {
+    when(count === 10.U) {
+      val startBit = buffer(0)
+      val stopBit  = ps2data
+
+      when(
+        startBit === 0.U &&
+          buffer(9, 1).xorR &&
+          stopBit === 1.U
+      ) {
+        fifo(wPtr)  := buffer(8, 1)
+        wPtr        := wPtr + 1.U
+        overflowReg := overflowReg || (wPtr + 1.U === rPtr)
+        readyReg    := true.B
+      }
+      count  := 0.U
+    }.otherwise {
+      buffer(count) := io.ps2data
+      count         := count + 1.U
+    }
+  }
+
+  io.data     := fifo(rPtr)
+  io.ready    := readyReg
+  io.overflow := overflowReg
+
 }
--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -2,7 +2,7 @@ package lab
 import chisel3._
 import chisel3.util._
 
-class top extends Module {
+class topp extends Module {
   val io = IO(new Bundle {
     val ps2clk  = Input(Bool())
     val ps2data = Input(Bool())
@@ -16,48 +16,40 @@ class top extends Module {
   rx.io.ps2clk  := io.ps2clk
   rx.io.ps2data := io.ps2data
 
-  val keyDown  = RegInit(false.B)
-  val lastCode = RegInit(0.U(8.W))
-  val sawF0    = RegInit(false.B)
+  val keydownReg = RegInit(false.B)
+
+  val gotByte = RegInit(false.B)
+  val dataReg = RegInit(0.U(8.W))
 
   when(rx.io.ready) {
-    val b = rx.io.data
-
-    when(sawF0) {
-      // 这一帧是 break 的“键码帧”，表示释放
-      sawF0    := false.B
-      keyDown  := false.B
-      lastCode := 0.U // 可选：清掉
-    }.elsewhen(b === "hF0".U) {
-      // break 前缀
-      sawF0 := true.B
-    }.otherwise {
-      // make code
-      lastCode := b
-      when(!keyDown) {
-        keyDown := true.B
-        // 这里如果要计数，也只在 !keyDown 时 +1
-      }.otherwise {
-        // 按住产生重复码：忽略（不计数不更新也行）
-        // 如果你想更新 lastCode 也可以，但不建议
-      }
-    }
+    rx.io.nextdata_n := false.B
+    gotByte          := true.B
+  }.otherwise {
+    rx.io.nextdata_n := true.B
   }
 
-  io.keydown := keyDown
+  when(gotByte) {
+    when(rx.io.data === "hF0".U) {
+      keydownReg := false.B
+    }.otherwise {
+      when(keydownReg === false.B) {
+        dataReg    := rx.io.data
+        keydownReg := true.B
 
-  when(keyDown) {
-    io.hex(0) := SevenSeg.encodeHex0toF(lastCode(3, 0), true.B)
-    io.hex(1) := SevenSeg.encodeHex0toF(lastCode(7, 4), true.B)
-  }.otherwise {
+      }
+    }
+    gotByte := false.B
+  }
+  io.keydown := keydownReg
+  when(keydownReg){
+    io.hex(0) := SevenSeg.encodeHex0toF(dataReg(3,0),true.B)
+    io.hex(1) := SevenSeg.encodeHex0toF(dataReg(7,4),true.B)
+  }.otherwise{
     io.hex(0) := SevenSeg.encodeHex0toF(0.U, false.B)
     io.hex(1) := SevenSeg.encodeHex0toF(0.U, false.B)
   }
-  // temp
-  val code = RegInit(0.U(8.W))
-  code      := rx.io.data
-  io.hex(2) := SevenSeg.encodeHex0toF(code(3, 0), true.B)
-  io.hex(3) := SevenSeg.encodeHex0toF(code(7, 4), true.B)
-  io.hex(4) := SevenSeg.encodeHex0toF(0.U, false.B)
-  io.hex(5) := SevenSeg.encodeHex0toF(0.U, false.B)
+  for(i <- 2 until 6){
+    io.hex(i) := SevenSeg.encodeHex0toF(0.U, false.B)
+  } 
+
 }
