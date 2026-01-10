e79bfdfcf9680367d0539c239f20c66fc7a1e6e9
tracer-ysyx <tracer@ysyx.org>
2026-01-11 01:43:01 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:43:01 up 20:40,  1 user,  load average: 0.55, 0.25, 0.19

--- npc/playground/src/VgaMem.scala
+++ npc/playground/src/VgaMem.scala
@@ -1,26 +1,24 @@
 package lab
 
 import chisel3._
-import chisel3.util._
-import chisel3.experimental.{BlackBox, HasBlackBoxInline}
+import chisel3.util.HasBlackBoxInline  // 注意：新版本里这个 trait 会被弃用，但 setInline 的能力 ExtModule 也有
+import chisel3.experimental.ExtModule
 
-class VgaMem(val DEPTH: Int = 524288) extends BlackBox with HasBlackBoxInline {
-  val io = IO(new Bundle {
-    val addr = Input(UInt(19.W))   // 524288 = 2^19
-    val data = Output(UInt(24.W))
-  })
+class VgaMem extends ExtModule with HasBlackBoxInline {
+  val addr = IO(Input(UInt(19.W)))
+  val data = IO(Output(UInt(24.W)))
 
   setInline("VgaMem.v",
-    s"""
-       |module VgaMem(
-       |  input  [18:0] addr,
-       |  output [23:0] data
-       |);
-       |  reg [23:0] vga_mem [0:${DEPTH - 1}];
-       |  initial begin
-       |    $$readmemh("resource/2.hex", vga_mem);
-       |  end
-       |  assign data = vga_mem[addr];
-       |endmodule
-       |""".stripMargin)
+    """
+      |module VgaMem(
+      |  input  [18:0] addr,
+      |  output [23:0] data
+      |);
+      |  reg [23:0] vga_mem [0:524287];
+      |  initial begin
+      |    $readmemh("resource/2.hex", vga_mem);
+      |  end
+      |  assign data = vga_mem[addr];
+      |endmodule
+      |""".stripMargin)
 }
