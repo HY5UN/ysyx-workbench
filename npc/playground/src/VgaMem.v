aa8c5085e629606376f5eb48c073adae19e07654
tracer-ysyx <tracer@ysyx.org>
2026-01-11 02:18:01 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:18:01 up 21:15,  1 user,  load average: 0.57, 0.38, 0.36

--- npc/playground/src/VgaMem.v
+++ npc/playground/src/VgaMem.v
@@ -2,7 +2,7 @@ module VgaMem(
   input  [18:0] addr,
   output [23:0] data
 );
-  reg [23:0] vga_mem [0:327679];
+  reg [23:0] vga_mem [0:327680];
   initial begin
     $readmemh("resource/2.hex", vga_mem);
   end
