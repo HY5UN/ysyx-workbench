26f0d9d8db70ed8b9d3dbfa53787b074d2c90c14
tracer-ysyx <tracer@ysyx.org>
2026-01-11 13:05:38 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:05:38 up 23:53,  1 user,  load average: 0.12, 0.26, 0.25

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -11,21 +11,18 @@ class top extends Module {
 
   val PC   = RegInit(0.U(4.W))
   val Regs = RegInit(VecInit(Seq.fill(4)(0.U(8.W))))
-  val ROM  = RegInit(
-    VecInit(
-      Seq(
-        "h8A".U(8.W),
-        "h90".U(8.W),
-        "hA0".U(8.W),
-        "hB1".U(8.W),
-        "h17".U(8.W),
-        "h29".U(8.W),
-        "hD1".U(8.W),
-        "h40".U(8.W)
-      )
+  val ROM  = VecInit(
+    Seq(
+      "b10001010".U(8.W),
+      "b10010000".U(8.W),
+      "b10100000".U(8.W),
+      "b10110001".U(8.W),
+      "b00010111".U(8.W),
+      "b00101001".U(8.W),
+      "b11010001".U(8.W),
+      "b01011111".U(8.W)
     )
   )
-
 
   val inst = ROM(PC)
 
