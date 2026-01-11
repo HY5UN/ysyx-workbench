3d40554aa1a947d9ddc6bf00e27096ea0a34dc39
tracer-ysyx <tracer@ysyx.org>
2026-01-11 13:03:23 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:03:23 up 23:51,  1 user,  load average: 0.11, 0.25, 0.24

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -5,48 +5,60 @@ import chisel3.util._
 
 class top extends Module {
   val io = IO(new Bundle {
-    val hex0 = Output( UInt(7.W))
-    val hex1 = Output( UInt(7.W))
+    val hex0 = Output(UInt(7.W))
+    val hex1 = Output(UInt(7.W))
   })
 
-  val PC = RegInit(0.U(4.W))
+  val PC   = RegInit(0.U(4.W))
   val Regs = RegInit(VecInit(Seq.fill(4)(0.U(8.W))))
-  val rom = Module(new sCPUROM)
-  rom.PC := PC
-  val inst = rom.inst
+  val ROM  = RegInit(
+    VecInit(
+      Seq(
+        "h8A".U(8.W),
+        "h90".U(8.W),
+        "hA0".U(8.W),
+        "hB1".U(8.W),
+        "h17".U(8.W),
+        "h29".U(8.W),
+        "hD1".U(8.W),
+        "h40".U(8.W)
+      )
+    )
+  )
+
+
+  val inst = ROM(PC)
 
   val inst_type = inst(7, 6)
 
-  when (inst_type === "b00".U) {
+  when(inst_type === "b00".U) {
     // add
     Regs(inst(5, 4)) := Regs(inst(3, 2)) + Regs(inst(1, 0))
-    PC := PC + 1.U
+    PC               := PC + 1.U
   }.elsewhen(inst_type === "b01".U) {
     // out rs
-    PC:=PC 
-  }
-  .elsewhen (inst_type === "b10".U) {
+    PC := PC
+  }.elsewhen(inst_type === "b10".U) {
     // li
     Regs(inst(5, 4)) := inst(3, 0)
-    PC := PC + 1.U
-  } .elsewhen (inst_type === "b11".U) {
-    // bner0 
-    when (Regs(inst(1,0)) =/= Regs(0)) {
+    PC               := PC + 1.U
+  }.elsewhen(inst_type === "b11".U) {
+    // bner0
+    when(Regs(inst(1, 0)) =/= Regs(0)) {
       PC := inst(5, 2)
-    } .otherwise {
+    }.otherwise {
       PC := PC + 1.U
     }
-  } .otherwise {
+  }.otherwise {
     PC := PC + 1.U
   }
 
   when(inst_type === "b01".U) {
-    io.hex0 := SevenSeg.encodeHex0toF(Regs(2)(3,0), true.B)
-    io.hex1 := SevenSeg.encodeHex0toF(Regs(2)(7,4), true.B)
-  } .otherwise {
+    io.hex0 := SevenSeg.encodeHex0toF(Regs(2)(3, 0), true.B)
+    io.hex1 := SevenSeg.encodeHex0toF(Regs(2)(7, 4), true.B)
+  }.otherwise {
     io.hex0 := SevenSeg.encodeHex0toF(0.U, false.B)
     io.hex1 := SevenSeg.encodeHex0toF(0.U, false.B)
   }
-
 
 }
