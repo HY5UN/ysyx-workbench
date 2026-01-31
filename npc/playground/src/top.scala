3eff09eaf3b0058f9ef9ecb9eaf61a0da4944df2
tracer-ysyx <tracer@ysyx.org>
2026-02-01 02:09:13 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:09:13 up  2:56,  1 user,  load average: 1.09, 0.71, 0.61

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -49,7 +49,7 @@ class top extends Module {
 
         }
         .otherwise{
-          
+
         }
       }
       is(sPressed){
@@ -123,8 +123,8 @@ class top extends Module {
     )
   )
 
-  // val isKeyDown = (state===sPressed)||(state===sWaitReleaseCode)
-  val isKeyDown = (state===sPressed)
+  val isKeyDown = (state===sPressed)||(state===sWaitReleaseCode)
+  // val isKeyDown = (state===sPressed)
   when(isKeyDown) {
     io.hex(0) := SevenSeg.encodeHex0toF(activeCode(3, 0), true.B)
     io.hex(1) := SevenSeg.encodeHex0toF(activeCode(7, 4), true.B)
