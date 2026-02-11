dee647de218de628970c9c5cbd0afbe79ba26897
tracer-ysyx <tracer@ysyx.org>
2026-02-11 22:18:47 +0800

    > sim RTL mode:sim ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:18:46 up  9:16,  1 user,  load average: 0.09, 0.08, 0.09

--- npc/csrc/sim-main.cpp
+++ npc/csrc/sim-main.cpp
@@ -1,15 +1,25 @@
-#include "Vour.h"
+#include "Vtop.h"
 #include "verilated.h"
-int main(int argc, char **argv)
-{
-    VerilatedContext *contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vour *top = new Vour{contextp};
-    while (!contextp->gotFinish())
-    {
-        top->eval();
-    }
-    delete top;
-    delete contextp;
-    return 0;
+
+int main(int argc, char **argv) {
+  VerilatedContext *contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop *top = new Vtop{contextp};
+  
+  top->clock = 0;
+  top->reset = 1;
+  top->eval();
+  contextp->timeInc(1);
+  
+  top->reset = 0;
+  
+  while (!contextp->gotFinish()) {
+    top->clock = !top->clock;
+    top->eval();
+    contextp->timeInc(1);
+  }
+
+  delete top;
+  delete contextp;
+  return 0;
 }
