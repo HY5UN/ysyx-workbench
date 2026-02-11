5aff7705b3d3a34d6c93bbe04fc525b0edefff95
tracer-ysyx <tracer@ysyx.org>
2026-02-12 00:28:21 +0800

    > sim RTL mode:sim ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:28:21 up 11:26,  1 user,  load average: 0.25, 0.19, 0.18

--- npc/csrc/sim-main.cpp
+++ npc/csrc/sim-main.cpp
@@ -121,11 +121,12 @@ int main(int argc, char **argv)
         {
             std::cout << ">>> 捕获到 Halt 信号 (PC=0xc)，仿真结束。" << std::endl;
             // 打印寄存器
-            // uint32_t *addr = (uint32_t *)&top->io_allReg_0;
-            // for (int i = 0; i < 32; i++)
-            // {
-            //     std::cout << "x" << i << ": " << std::hex << addr[i] << std::dec << std::endl;
-            // }
+            uint32_t *addr = (uint32_t *)&top->io_allReg_0;
+            for (int i = 0; i < 32; i++)
+            {
+                std::cout << "x" << i << ": " << std::hex << addr[i] << std::dec << std::endl;
+            }
+            
             break;
         }
     }
