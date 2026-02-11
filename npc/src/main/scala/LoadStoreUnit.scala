b9eb664c9a291b36beed821eed70b2faa80bf020
tracer-ysyx <tracer@ysyx.org>
2026-02-11 21:00:52 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:00:52 up  7:58,  1 user,  load average: 0.01, 0.08, 0.14

---
+++ npc/src/main/scala/LoadStoreUnit.scala
@@ -0,0 +1,26 @@
+package top
+import chisel3._
+import chisel3.util._
+
+class MemIO extends Bundle {
+  val wen   = Output(Bool())      // 写使能
+  val addr  = Output(UInt(32.W))  // 地址 (LSU -> Mem)
+  val wdata = Output(UInt(32.W))  // 写数据 (LSU -> Mem)
+  val rdata = Input(UInt(32.W))   // 读数据 (Mem -> LSU)
+}
+
+class LoadStoreUnit extends Module {
+  val io = IO(new Bundle {
+    val addr = Input(UInt(32.W))
+    val wdata = Input(UInt(32.W))
+    val rdata = Output(UInt(32.W))
+    val wen = Input(Bool())
+    val dmem = new MemIO
+  })
+
+  io.dmem.addr := io.addr
+  io.dmem.wdata := io.wdata
+  io.dmem.wen := io.wen
+  io.rdata := io.dmem.rdata
+
+}
