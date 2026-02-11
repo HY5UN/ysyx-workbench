fca13df4aab19fea72cf6690faadaee8d6ead344
tracer-ysyx <tracer@ysyx.org>
2026-02-11 13:20:06 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:20:06 up 18 min,  1 user,  load average: 0.09, 0.10, 0.09

--- npc/playground/src/VgaMem.scala
+++
@@ -1,8 +0,0 @@
-package lab
-import chisel3._
-import chisel3.experimental.ExtModule
-
-class VgaMem extends ExtModule {
-  val addr = IO(Input(UInt(19.W)))
-  val data = IO(Output(UInt(24.W)))
-}
--- npc/playground/src/sCPUROM.scala
+++
@@ -1,8 +0,0 @@
-package lab
-import chisel3._
-import chisel3.experimental.ExtModule
-
-class sCPUROM extends ExtModule {
-  val PC = IO(Input(UInt(4.W)))
-  val inst = IO(Output(UInt(8.W)))
-}
