ba3053875d8e59662b1dd0790f52415549628587
tracer-ysyx <tracer@ysyx.org>
2026-02-21 23:47:33 +1100

    > compile NEMU ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:47:33 up 1 day, 23:10,  1 user,  load average: 0.26, 0.13, 0.04

--- nemu/src/utils/traces.c
+++ nemu/src/utils/traces.c
@@ -214,6 +214,11 @@ void init_ftrace_log(char *log_file)
     strcat(new_str, dot);
     ftrace_log_file = new_str;
   }
+  FILE *fp = fopen(ftrace_log_file, "w");
+  if (fp != NULL)
+  {
+    fclose(fp);
+  }
 }
 
 static void write_ftrace_log(const char *format, ...)
@@ -296,11 +301,11 @@ void ftrace_record(word_t pc, int rd)
       indent_level--;
       if (indent_level < 0)
         indent_level = 0;
-      write_ftrace_log("" FMT_WORD " %*s return %s\n", pc, indent_level * 2, "", curr_func);
+      write_ftrace_log("" FMT_WORD " %*s return <%s>\n", pc, indent_level * 2, "", curr_func);
     }
     else // call
     {
-      write_ftrace_log("" FMT_WORD " %*s call %s\n", pc, indent_level * 2, "", curr_func);
+      write_ftrace_log("" FMT_WORD " %*s call <%s> @" FMT_WORD "\n", pc, indent_level * 2, "", curr_func, func_symbols[i].addr_begin);
       indent_level++;
     }
   }
