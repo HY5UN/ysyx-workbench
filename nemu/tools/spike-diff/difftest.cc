86a2376acdad66c2a6a5c76673fea8aaf7e00599
tracer-ysyx <tracer@ysyx.org>
2026-02-24 13:49:43 +1100

    > compile NEMU ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:49:43 up  7:19,  1 user,  load average: 0.34, 0.20, 0.14

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -15,8 +15,16 @@
 
 #include "mmu.h"
 #include "sim.h"
-#include "../../include/common.h"
+//#include "../../include/common.h"
 #include <difftest-def.h>
+#include <macro.h>
+
+
+typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
+typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
+typedef word_t vaddr_t;
+typedef MUXDEF(PMEM64, uint64_t, uint32_t) paddr_t;
+
 
 #define NR_GPR MUXDEF(CONFIG_RVE, 16, 32)
 
