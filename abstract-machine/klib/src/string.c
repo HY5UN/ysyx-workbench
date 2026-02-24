c9a4ea70f8db68bd8886b44eb3f37338f7ba2080
tracer-ysyx <tracer@ysyx.org>
2026-02-25 00:29:10 +1100

    > compile NEMU ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:29:10 up 16:37,  1 user,  load average: 0.20, 0.17, 0.11

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -63,6 +63,7 @@ void *memset(void *s, int c, size_t n) {
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
+
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
