ec914dabb79507d7b236c55a2feeee9993d77eed
tracer-ysyx <tracer@ysyx.org>
2026-02-24 13:27:04 +1100

    > compile NEMU ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:27:04 up  6:56,  1 user,  load average: 0.32, 0.09, 0.03

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,18 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool success = true;
+  for (int i=0;i<MUXDEF(CONFIG_RVE, 16, 32);i++){
+    if(ref_r->gpr[i] != cpu.gpr[i]){
+      Log("reg x%d is different at pc " FMT_WORD ", ref: " FMT_WORD ", dut: " FMT_WORD, i, pc, ref_r->gpr[i], cpu.gpr[i]);
+      success = false;
+    }
+  }
+
+  return success;
 }
 
 void isa_difftest_attach() {
