26dcca4e78424405ed35c5d1dc4a22f1cd43687b
tracer-ysyx <tracer@ysyx.org>
2026-02-28 19:50:21 +1100

    > sim RTL mode:sim ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:50:21 up 17:22,  1 user,  load average: 0.46, 0.35, 0.32

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -40,10 +40,10 @@ int mem_read(int addr)
     }
 
 #ifdef ENABLE_MTRACE
-    // if (cpu->top->io_pc != addr)
-    // {
-    //     mtrace_write_r(addr, data);
-    // }
+    if (cpu->top->io_pc != addr)
+    {
+        //mtrace_write_r(addr, data);
+    }
 #endif
 
     return data;
