313fa850c69a2517be12da4781102811de4983bd
tracer-ysyx <tracer@ysyx.org>
2026-02-28 14:47:37 +1100

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:47:37 up 12:19,  1 user,  load average: 0.37, 0.34, 0.25

--- npc/csrc/include/mem.h
+++ npc/csrc/include/mem.h
@@ -3,6 +3,6 @@
 
 int mem_read(int addr);
 void mem_write(int addr, int data, char wmask);
-int mem_print(int addr, int len);   
+int mem_print(uint32_t addr, int len);   
 
 void load_binary(const std::string &filename);
