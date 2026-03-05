ccf3205ddd9a5926de6d15742b80a7166b540efd
tracer-ysyx <tracer@ysyx.org>
2026-03-05 23:56:49 +1100

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:56:49 up  1:51,  1 user,  load average: 0.67, 0.48, 0.36

--- npc/src/main/scala/InstDecoder/Control.scala
+++ npc/src/main/scala/InstDecoder/Control.scala
@@ -5,95 +5,77 @@ import chisel3.util._
 import chisel3.util.experimental.decode._
 
 object ControlConstants {
-  val X = BitPat.dontCare(1)
+  val X = "b0".U
   // bool
-  val Y = BitPat("b1")
-  val N = BitPat("b0")
+  val Y = "b1".U
+  val N = "b0".U
 
   // immSel
-  val IMM_X = BitPat.dontCare(3)
-  val IMM_I = BitPat("b000")
-  val IMM_S = BitPat("b001")
-  val IMM_B = BitPat("b010")
-  val IMM_U = BitPat("b011")
-  val IMM_J = BitPat("b100")
+  val IMM_I = "b000".U
+  val IMM_S = "b001".U
+  val IMM_B = "b010".U
+  val IMM_U = "b011".U
+  val IMM_J = "b100".U
 
   // aluOp
-  val ALU_X   = BitPat.dontCare(4)
-  val ALU_ADD = BitPat("b0000")
-  val ALU_SUB = BitPat("b0001")
-  val ALU_XOR = BitPat("b0010")
-  val ALU_OR  = BitPat("b0011")
-  val ALU_AND = BitPat("b0100")
-  val ALU_LL  = BitPat("b0101")
-  val ALU_RL  = BitPat("b0110")
-  val ALU_RA  = BitPat("b0111")
-  val ALU_LT  = BitPat("b1000")
-  val ALU_LTU = BitPat("b1001")
-  val ALU_EQ  = BitPat("b1010")
-  val ALU_NEQ = BitPat("b1011")
-  val ALU_GE  = BitPat("b1100")
-  val ALU_GEU = BitPat("b1101")
+  val ALU_ADD = "b0000".U
+  val ALU_SUB = "b0001".U
+  val ALU_XOR = "b0010".U
+  val ALU_OR  = "b0011".U
+  val ALU_AND = "b0100".U
+  val ALU_LL  = "b0101".U
+  val ALU_RL  = "b0110".U
+  val ALU_RA  = "b0111".U
+  val ALU_LT  = "b1000".U
+  val ALU_LTU = "b1001".U
+  val ALU_EQ  = "b1010".U
+  val ALU_NEQ = "b1011".U
+  val ALU_GE  = "b1100".U
+  val ALU_GEU = "b1101".U
 
   // opSel
-  val OP_X    = BitPat.dontCare(2)
-  val OP1_RS1 = BitPat("b0")
-  val OP1_PC  = BitPat("b1")
-  val OP2_RS2 = BitPat("b0")
-  val OP2_IMM = BitPat("b1")
+  val OP1_RS1 = "b0".U
+  val OP1_PC  = "b1".U
+  val OP2_RS2 = "b0".U
+  val OP2_IMM = "b1".U
 
   // rdSel
-  val RD_X   = BitPat.dontCare(2)
-  val RD_ALU = BitPat("b00")
-  val RD_MEM = BitPat("b01")
-  val RD_PC4 = BitPat("b10")
-  val RD_IMM = BitPat("b11")
+  val RD_ALU = "b00".U
+  val RD_MEM = "b01".U
+  val RD_PC4 = "b10".U
+  val RD_IMM = "b11".U
 
   // memLen
-  val MEM_X    = BitPat.dontCare(2)
-  val LEN_BYTE = BitPat("b00")
-  val LEN_HALF = BitPat("b01")
-  val LEN_WORD = BitPat("b10")
+  val LEN_BYTE = "b00".U
+  val LEN_HALF = "b01".U
+  val LEN_WORD = "b10".U
 
   // pcSel
-  val PC_X      = BitPat.dontCare(2)
-  val PC_4      = BitPat("b00")
-  val PC_ALU    = BitPat("b01") // pc + imm
-  val PC_ALU1   = BitPat("b10") // ALU结果低位清0，用于jalr
-  val PC_BRANCH = BitPat("b11") // 分支指令，根据比较结果选择pc+4或pc+imm
+  val PC_4      = "b00".U
+  val PC_ALU    = "b01".U // pc + imm
+  val PC_ALU1   = "b10".U // ALU结果低位清0，用于jalr
+  val PC_BRANCH = "b11".U // 分支指令，根据比较结果选择pc+4或pc+imm
 
 }
 
 import ControlConstants._
 
-class CtrlBundle extends Bundle {
-  val immSel  = UInt(3.W)
-  val aluOp   = UInt(4.W)
-  val op1Sel  = UInt(2.W)
-  val op2Sel  = UInt(2.W)
-  val rdSel   = UInt(2.W)
-  val regWen  = Bool()
-  val memR    = Bool()
-  val memWen  = Bool()
-  val memLen  = UInt(2.W)
-  val memSext = Bool()
-  val pcSel   = UInt(2.W)
-  val ebreak  = Bool()
-}
 
 case class Ctrl(
-  immSel:  BitPat = IMM_X,
-  aluOp:   BitPat = ALU_X,
-  op1Sel:  BitPat = OP_X,
-  op2Sel:  BitPat = OP_X,
-  rdSel:   BitPat = RD_X,
-  regWen:  BitPat = N,
-  memR:    BitPat = N,
-  memWen:  BitPat = N,
-  memLen:  BitPat = MEM_X,
-  memSext: BitPat = X,
-  pcSel:   BitPat = PC_X,
-  ebreak: BitPat = N) {
-  def toBitPat: BitPat =
-    immSel ## aluOp ## op1Sel ## op2Sel ## rdSel ## regWen ## memR ## memWen ## memLen ## memSext ## pcSel ## ebreak
+  immSel:  UInt = X,
+  aluOp:   UInt = X,
+  op1Sel:  UInt = X,
+  op2Sel:  UInt = X,
+  rdSel:   UInt = X,
+  regWen:  UInt = N,
+  memR:    UInt = N,
+  memWen:  UInt = N,
+  memLen:  UInt = X,
+  memSext: UInt = N,
+  pcSel:   UInt = X,
+  ebreak: UInt = N) {
+
+  def toList: List[UInt] =
+    List(immSel, aluOp, op1Sel, op2Sel, rdSel, regWen, memR, memWen, memLen, memSext, pcSel, ebreak)
+
 }
--- npc/src/main/scala/InstDecoder/InstDecoder.scala
+++ npc/src/main/scala/InstDecoder/InstDecoder.scala
@@ -2,11 +2,9 @@ package top
 
 import chisel3._
 import chisel3.util._
-import chisel3.util.experimental.decode._
 
 import ControlConstants._
 import RV32EInstr._
-
 
 class RV32EDecoder extends Module {
   val io     = IO(new Bundle {
@@ -42,100 +40,91 @@ class RV32EDecoder extends Module {
   val immU = Cat(io.inst(31, 12), 0.U(12.W)).asSInt.pad(32).asUInt
   val immJ = Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W)).asSInt.pad(32).asUInt
 
-  val defaultCtrl = Ctrl().toBitPat
+  val defaultCtrl = Ctrl().toList
   val baseR       = Ctrl(op1Sel = OP1_RS1, op2Sel = OP2_RS2, rdSel = RD_ALU, regWen = Y)
   val baseI       = Ctrl(immSel = IMM_I, op1Sel = OP1_RS1, op2Sel = OP2_IMM, rdSel = RD_ALU, regWen = Y)
   val baseLoad    = Ctrl(immSel = IMM_I, op1Sel = OP1_RS1, op2Sel = OP2_IMM, rdSel = RD_MEM, regWen = Y, memR = Y, aluOp = ALU_ADD)
   val baseStore   = Ctrl(immSel = IMM_S, op1Sel = OP1_RS1, op2Sel = OP2_IMM, memWen = Y, aluOp = ALU_ADD)
   val baseBranch  = Ctrl(immSel = IMM_B, op1Sel = OP1_RS1, op2Sel = OP2_RS2, pcSel = PC_BRANCH)
 
-  val decodeTable = TruthTable(
-    
-    Map(
+  val decodeTable = Array(
     // R-type
-    ADD  -> baseR.copy(aluOp = ALU_ADD).toBitPat,
-    SUB  -> baseR.copy(aluOp = ALU_SUB).toBitPat,
-    XOR  -> baseR.copy(aluOp = ALU_XOR).toBitPat,
-    OR   -> baseR.copy(aluOp = ALU_OR).toBitPat,
-    AND  -> baseR.copy(aluOp = ALU_AND).toBitPat,
-    SLL  -> baseR.copy(aluOp = ALU_LL).toBitPat,
-    SRL  -> baseR.copy(aluOp = ALU_RL).toBitPat,
-    SRA  -> baseR.copy(aluOp = ALU_RA).toBitPat,
-    SLT  -> baseR.copy(aluOp = ALU_LT).toBitPat,
-    SLTU -> baseR.copy(aluOp = ALU_LTU).toBitPat,
+    ADD  -> baseR.copy(aluOp = ALU_ADD).toList,
+    SUB  -> baseR.copy(aluOp = ALU_SUB).toList,
+    XOR  -> baseR.copy(aluOp = ALU_XOR).toList,
+    OR   -> baseR.copy(aluOp = ALU_OR).toList,
+    AND  -> baseR.copy(aluOp = ALU_AND).toList,
+    SLL  -> baseR.copy(aluOp = ALU_LL).toList,
+    SRL  -> baseR.copy(aluOp = ALU_RL).toList,
+    SRA  -> baseR.copy(aluOp = ALU_RA).toList,
+    SLT  -> baseR.copy(aluOp = ALU_LT).toList,
+    SLTU -> baseR.copy(aluOp = ALU_LTU).toList,
 
     // I-type
-    ADDI  -> baseI.copy(aluOp = ALU_ADD).toBitPat,
-    ANDI  -> baseI.copy(aluOp = ALU_AND).toBitPat,
-    ORI   -> baseI.copy(aluOp = ALU_OR).toBitPat,
-    XORI  -> baseI.copy(aluOp = ALU_XOR).toBitPat,
-    SLLI  -> baseI.copy(aluOp = ALU_LL).toBitPat,
-    SRLI  -> baseI.copy(aluOp = ALU_RL).toBitPat,
-    SRAI  -> baseI.copy(aluOp = ALU_RA).toBitPat,
-    SLTI  -> baseI.copy(aluOp = ALU_LT).toBitPat,
-    SLTIU -> baseI.copy(aluOp = ALU_LTU).toBitPat,
+    ADDI  -> baseI.copy(aluOp = ALU_ADD).toList,
+    ANDI  -> baseI.copy(aluOp = ALU_AND).toList,
+    ORI   -> baseI.copy(aluOp = ALU_OR).toList,
+    XORI  -> baseI.copy(aluOp = ALU_XOR).toList,
+    SLLI  -> baseI.copy(aluOp = ALU_LL).toList,
+    SRLI  -> baseI.copy(aluOp = ALU_RL).toList,
+    SRAI  -> baseI.copy(aluOp = ALU_RA).toList,
+    SLTI  -> baseI.copy(aluOp = ALU_LT).toList,
+    SLTIU -> baseI.copy(aluOp = ALU_LTU).toList,
 
     // Loads
-    LB  -> baseLoad.copy(memLen = LEN_BYTE, memSext = Y).toBitPat,
-    LH  -> baseLoad.copy(memLen = LEN_HALF, memSext = Y).toBitPat,
-    LW  -> baseLoad.copy(memLen = LEN_WORD, memSext = N).toBitPat,
-    LBU -> baseLoad.copy(memLen = LEN_BYTE, memSext = N).toBitPat,
-    LHU -> baseLoad.copy(memLen = LEN_HALF, memSext = N).toBitPat,
+    LB  -> baseLoad.copy(memLen = LEN_BYTE, memSext = Y).toList,
+    LH  -> baseLoad.copy(memLen = LEN_HALF, memSext = Y).toList,
+    LW  -> baseLoad.copy(memLen = LEN_WORD, memSext = N).toList,
+    LBU -> baseLoad.copy(memLen = LEN_BYTE, memSext = N).toList,
+    LHU -> baseLoad.copy(memLen = LEN_HALF, memSext = N).toList,
 
     // Stores
-    SB -> baseStore.copy(memLen = LEN_BYTE).toBitPat,
-    SH -> baseStore.copy(memLen = LEN_HALF).toBitPat,
-    SW -> baseStore.copy(memLen = LEN_WORD).toBitPat,
+    SB -> baseStore.copy(memLen = LEN_BYTE).toList,
+    SH -> baseStore.copy(memLen = LEN_HALF).toList,
+    SW -> baseStore.copy(memLen = LEN_WORD).toList,
 
     // Branches
-    BEQ  -> baseBranch.copy(aluOp = ALU_EQ).toBitPat,
-    BNE  -> baseBranch.copy(aluOp = ALU_NEQ).toBitPat,
-    BLT  -> baseBranch.copy(aluOp = ALU_LT).toBitPat,
-    BGE  -> baseBranch.copy(aluOp = ALU_GE).toBitPat,
-    BLTU -> baseBranch.copy(aluOp = ALU_LTU).toBitPat,
-    BGEU -> baseBranch.copy(aluOp = ALU_GEU).toBitPat,
+    BEQ  -> baseBranch.copy(aluOp = ALU_EQ).toList,
+    BNE  -> baseBranch.copy(aluOp = ALU_NEQ).toList,
+    BLT  -> baseBranch.copy(aluOp = ALU_LT).toList,
+    BGE  -> baseBranch.copy(aluOp = ALU_GE).toList,
+    BLTU -> baseBranch.copy(aluOp = ALU_LTU).toList,
+    BGEU -> baseBranch.copy(aluOp = ALU_GEU).toList,
 
     // U-type
-    LUI   -> Ctrl(immSel = IMM_U, rdSel = RD_IMM, regWen = Y).toBitPat,
-    AUIPC -> Ctrl(immSel = IMM_U, op1Sel = OP1_PC, op2Sel = OP2_IMM, rdSel = RD_ALU, regWen = Y, aluOp = ALU_ADD).toBitPat,
+    LUI   -> Ctrl(immSel = IMM_U, rdSel = RD_IMM, regWen = Y).toList,
+    AUIPC -> Ctrl(immSel = IMM_U, op1Sel = OP1_PC, op2Sel = OP2_IMM, rdSel = RD_ALU, regWen = Y, aluOp = ALU_ADD).toList,
 
     // Jumps
-    JAL  -> Ctrl(immSel = IMM_J, regWen = Y, rdSel = RD_PC4, pcSel = PC_ALU, op1Sel = OP1_PC, op2Sel = OP2_IMM, aluOp = ALU_ADD).toBitPat,
-    JALR -> Ctrl(immSel = IMM_I, op1Sel = OP1_RS1, op2Sel = OP2_IMM, rdSel = RD_PC4, regWen = Y, aluOp = ALU_ADD, pcSel = PC_ALU1).toBitPat,
+    JAL  -> Ctrl(immSel = IMM_J, regWen = Y, rdSel = RD_PC4, pcSel = PC_ALU, op1Sel = OP1_PC, op2Sel = OP2_IMM, aluOp = ALU_ADD).toList,
+    JALR -> Ctrl(immSel = IMM_I, op1Sel = OP1_RS1, op2Sel = OP2_IMM, rdSel = RD_PC4, regWen = Y, aluOp = ALU_ADD, pcSel = PC_ALU1).toList,
 
     // SYSTEM
-    EBREAK -> Ctrl(ebreak = Y).toBitPat
-    ),
-    defaultCtrl
+    EBREAK -> Ctrl(ebreak = Y).toList
   )
-  //val ctrlSignals =ListLookup(io.inst, defaultCtrl, decodeTable)
-  //val immSel::aluOp::op1Sel::op2Sel::rdSel::regWen::memR::memWen::memLen::memSext::pcSel::ebreak::Nil = ctrlSignals
+  val ctrlSignals =ListLookup(io.inst, defaultCtrl, decodeTable)
+  val immSel::aluOp::op1Sel::op2Sel::rdSel::regWen::memR::memWen::memLen::memSext::pcSel::ebreak::Nil = ctrlSignals
 
-  val decodedBits = decoder(io.inst, decodeTable)
-  val ctrl = decodedBits.asTypeOf(new CtrlBundle)
-
-
-  io.imm := MuxCase(0.U,
-  Array(
-    (ctrl.immSel === IMM_I) -> immI,
-    (ctrl.immSel === IMM_S) -> immS,
-    (ctrl.immSel === IMM_B) -> immB,
-    (ctrl.immSel === IMM_U) -> immU,
-    (ctrl.immSel === IMM_J) -> immJ
+  io.imm    := MuxLookup(immSel, 0.U)(
+    Seq(
+      IMM_I -> immI,
+      IMM_S -> immS,
+      IMM_B -> immB,
+      IMM_U -> immU,
+      IMM_J -> immJ
+    )
   )
-)
-
-  io.aluOp   := ctrl.aluOp
-  io.op1Sel  := ctrl.op1Sel
-  io.op2Sel  := ctrl.op2Sel
-  io.rdSel   := ctrl.rdSel
-  io.regWen  := ctrl.regWen
-  io.memReadValid := ctrl.memR
-  io.memWen  := ctrl.memWen
-  io.memLen  := ctrl.memLen
-  io.memSext := ctrl.memSext
-  io.pcSel   := ctrl.pcSel
-  io.ebreak  := ctrl.ebreak
+  io.aluOp   := aluOp
+  io.op1Sel  := op1Sel  
+  io.op2Sel  := op2Sel
+  io.rdSel   := rdSel
+  io.regWen  := regWen
+  io.memReadValid := memR
+  io.memWen  := memWen
+  io.memLen  := memLen
+  io.memSext := memSext
+  io.pcSel   := pcSel
+  io.ebreak  := ebreak
 
   io.rs1 := rs1
   io.rs2 := rs2
