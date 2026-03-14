89c95b244410b11bf26f9be2f7b924dda465fb9f
tracer-ysyx <tracer@ysyx.org>
2026-03-15 03:39:05 +1100

    > sim RTL mode:sim ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:39:05 up  7:20,  1 user,  load average: 0.23, 0.21, 0.17

--- npc/csrc/difftest/difftest.cpp
+++ npc/csrc/difftest/difftest.cpp
@@ -1,43 +1,48 @@
 #include "include/difftest.h"
 #include "include/CPU.h"
 
-DiffTest::DiffTest()  {
+DiffTest::DiffTest()
+{
     handle = dlopen("/home/hy5un/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so", RTLD_LAZY);
-    if (!handle) {
+    if (!handle)
+    {
         fprintf(stderr, "dlopen error: %s\n", dlerror());
         exit(1);
     }
 
     difftest_memcpy = (difftest_memcpy_t)dlsym(handle, "difftest_memcpy");
     difftest_regcpy = (difftest_regcpy_t)dlsym(handle, "difftest_regcpy");
-    difftest_exec   = (difftest_exec_t)dlsym(handle, "difftest_exec");
-    difftest_init   = (difftest_init_t)dlsym(handle, "difftest_init");
+    difftest_exec = (difftest_exec_t)dlsym(handle, "difftest_exec");
+    difftest_init = (difftest_init_t)dlsym(handle, "difftest_init");
 
-    if (!difftest_memcpy || !difftest_regcpy || !difftest_exec) {
+    if (!difftest_memcpy || !difftest_regcpy || !difftest_exec)
+    {
         fprintf(stderr, "dlsym error: %s\n", dlerror());
         dlclose(handle);
         exit(1);
     }
-
 }
 
-
-DiffTest::~DiffTest() {
+DiffTest::~DiffTest()
+{
     dlclose(handle);
-
 }
 
-bool difftest_mmio_skip=false;
+bool difftest_mmio_skip = false;
+int step_count = 0;
+void DiffTest::step()
+{
 
-void DiffTest::step() {
-
-   
-    if(difftest_mmio_skip)
+    if (difftest_mmio_skip)
     {
-        printf("Skipping difftest check for MMIO access at pc 0x%08x\n", cpu->top->io_pc);
-        difftest_mmio_skip=false;
+        // printf("Skipping difftest check for MMIO access at pc 0x%08x\n", cpu->top->io_pc);
+        if (step_count++ > 1)
+        {
+            difftest_mmio_skip = false;
+        }
         dut_CPU_state.pc = cpu->top->io_pc;
-        for (int i = 0; i < REG_NUM; i++) {
+        for (int i = 0; i < REG_NUM; i++)
+        {
             assert(i < 32);
             dut_CPU_state.gpr[i] = ((word_t *)&cpu->top->io_allReg_0)[i];
         }
@@ -45,19 +50,19 @@ void DiffTest::step() {
         return;
     }
 
-
     difftest_exec(1);
     difftest_regcpy(&ref_CPU_state, DIFFTEST_TO_DUT);
 
     word_t *gpr = (word_t *)&cpu->top->io_allReg_0;
 
-    for (int i = 0; i < REG_NUM; i++) {
-        if (gpr[i] != ref_CPU_state.gpr[i]) {
+    for (int i = 0; i < REG_NUM; i++)
+    {
+        if (gpr[i] != ref_CPU_state.gpr[i])
+        {
             printf("GPR x%d mismatch at pc 0x%08x: DUT=0x%08x, REF=0x%08x\n", i, ref_CPU_state.pc, gpr[i], ref_CPU_state.gpr[i]);
 
             cpu->reg_print();
             exit(1);
         }
     }
-
 }
