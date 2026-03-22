43d9cf4afa0c83c93b569f5dc70bdddb2c7d953a
tracer-ysyx <tracer@ysyx.org>
2026-03-22 21:30:59 +1100

    > compile NEMU ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:30:59 up 1 day,  3:56,  1 user,  load average: 0.15, 0.17, 0.12

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -52,7 +52,15 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++) {
+    if (s1[i] != s2[i]) {
+      return (unsigned char)s1[i] - (unsigned char)s2[i];
+    }
+    if (s1[i] == '\0') {
+      return 0;
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
