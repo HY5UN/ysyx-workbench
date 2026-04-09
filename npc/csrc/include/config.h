c92f36ee34e9284823dcfa2fa2ddf5d1052ff6b0
tracer-ysyx <tracer@ysyx.org>
2026-04-09 22:37:49 +1000

    > sim RTL mode:sim ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:37:49 up  5:35,  1 user,  load average: 0.23, 0.40, 0.50

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -1,8 +1,8 @@
-#define ENABLE_DIFFTEST
-#define ENABLE_ITRACE
-#define ENABLE_FTRACE
+// #define ENABLE_DIFFTEST
+// #define ENABLE_ITRACE
+// #define ENABLE_FTRACE
 
-#define ENABLE_FST
-#define MAX_SIM_TIME 100000
+// #define ENABLE_FST
+// #define MAX_SIM_TIME 100000
 
 #define BATCH_MODE
