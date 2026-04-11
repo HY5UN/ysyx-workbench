0bd212e50919cc4f19dd3e2091b9f5a1da664413
tracer-ysyx <tracer@ysyx.org>
2026-04-11 21:57:55 +1000

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:57:55 up  7:20,  1 user,  load average: 0.14, 0.18, 0.27

--- npc/src/main/verilog/InstFetchUnit.v
+++ npc/src/main/verilog/InstFetchUnit.v
@@ -1,10 +1,27 @@
+// module InstFetchUnitExt (
+//     input [31:0] io_pc,
+//     output [31:0] io_inst
+// );
+
+//     import "DPI-C" function int mem_read(input int addr);
+
+//     assign io_inst = mem_read(io_pc);
+
+// endmodule
+
 module InstFetchUnitExt (
-    input [31:0] io_pc,
+    input  [31:0] io_pc,
     output [31:0] io_inst
 );
+    // import "DPI-C" function int mem_read(input int addr);
+    // assign io_inst = mem_read(io_pc);
 
-    import "DPI-C" function int mem_read(input int addr);
-
-    assign io_inst = mem_read(io_pc);
-
+    Memory256x32 ifu_mem (
+        .clock  (1'b0),          // IFU只读，clock可接顶层clock，写端口悬空
+        .wen    (1'b0),
+        .wmask  (4'b0),
+        .addr   (io_pc[9:2]),    // 字节地址→字地址，取[9:2]覆盖1KB空间
+        .wdata  (32'b0),
+        .rdata  (io_inst)
+    );
 endmodule
