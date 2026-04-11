804facb02215e7955c869356b770b0946048ba0f
tracer-ysyx <tracer@ysyx.org>
2026-04-11 21:35:10 +1000

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:35:10 up  6:57,  1 user,  load average: 0.48, 0.51, 0.32

--- npc/src/main/scala/top.scala
+++ npc/src/main/scala/top.scala
@@ -60,9 +60,12 @@ object StageConnect {
   def apply[T <: Data](left: DecoupledIO[T], right: DecoupledIO[T]) = {
     val arch = "single"
     // 为展示抽象的思想, 此处代码省略了若干细节
-    if      (arch == "single")   { right.bits := left.bits }
-    else if (arch == "multi")    { right <> left }
+    if (arch == "single") {
+      right.bits  := left.bits
+      right.valid := true.B
+      left.ready  := true.B
+    } else if (arch == "multi") { right <> left }
     else if (arch == "pipeline") { right <> RegEnable(left, left.fire) }
-    else if (arch == "ooo")      { right <> Queue(left, 16) }
+    else if (arch == "ooo") { right <> Queue(left, 16) }
   }
 }
