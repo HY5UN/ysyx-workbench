decd90ff1937636303a4ecd5b1d88ed5e11f88c1
tracer-ysyx <tracer@ysyx.org>
2026-04-25 00:09:27 +1000

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:09:27 up  2:04,  1 user,  load average: 0.04, 0.11, 0.13

--- npc/src/main/scala/1_IFU.scala
+++ npc/src/main/scala/1_IFU.scala
@@ -23,7 +23,6 @@ class InstFetchUnit extends Module {
   val pc       = RegInit("h80000000".U(32.W))
   val ifuRdata = RegInit(0.U(32.W))
   val state    = RegInit(State.sIdle)
-  val valid    = RegInit(true.B)
 
   val ifu = Module(new InstFetchUnitExt())
   ifu.io.pc := pc
@@ -31,21 +30,19 @@ class InstFetchUnit extends Module {
   switch(state) {
     // 空闲状态:已取出指令,等待新的有效地址
     is(State.sIdle) {
-      when(io.in.valid) {
+      when(io.in.valid||pc === "h80000000".U) {
         state := State.sWait
         pc    := io.in.bits.nextPC
-        valid := false.B
       }
     }
     // 等待状态:等待指令返回,准备输出
     is(State.sWait) {
       state    := State.sIdle
       ifuRdata := ifu.io.inst
-      valid    := true.B
     }
 
   }
-  io.out.valid := valid
+  io.out.valid := state === State.sIdle
   io.in.ready  := state === State.sIdle
 
   io.out.bits.inst := ifuRdata
