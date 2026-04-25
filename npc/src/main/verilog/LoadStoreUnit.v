64c4477d0a4960c88c0f80754083b723e3972124
tracer-ysyx <tracer@ysyx.org>
2026-04-25 13:02:46 +1000

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:02:46 up 14:57,  1 user,  load average: 0.16, 0.25, 0.18

--- npc/src/main/verilog/LoadStoreUnit.v
+++ npc/src/main/verilog/LoadStoreUnit.v
@@ -12,13 +12,7 @@ module MemExt (
     import "DPI-C" function int  mem_read(input int addr);
     import "DPI-C" function void mem_write(input int addr, input int data, input byte wmask);
 
-    // assign io_rdata = io_rvalid ? mem_read(io_addr) : 32'h0;
-
-    // always @(posedge io_clock) begin
-    //     if(io_wen) begin
-    //         mem_write(io_addr, io_wdata, {4'b0, io_wmask});
-    //     end
-    // end
+    reg [3:0] delayCounter;
 
 
 
@@ -30,7 +24,13 @@ module MemExt (
             end 
 
         end
-        io_respValid <= io_reqValid;
+        delayCounter <= delayCounter + 1;
+        if(delayCounter == 4) begin
+            io_respValid <= 1'b1;
+        end 
+        else begin
+            io_respValid <= 1'b0;
+        end
 
     end
     
