bf7a0a6305c9c7c03182bdb7ecc75860e172dd43
tracer-ysyx <tracer@ysyx.org>
2026-06-03 23:31:38 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:31:38 up  3:02,  1 user,  load average: 0.64, 0.52, 0.41

--- npc/src/main/scala/4_LSU.scala
+++ npc/src/main/scala/4_LSU.scala
@@ -11,19 +11,14 @@ class LoadStoreUnit extends Module {
   val ctrl = io.in.bits.ctrl
 
   object State extends ChiselEnum {
-    val sIdle, sDelay, sWait = Value
+    val sIdle, sWait, sFinish = Value
   }
   val state = RegInit(State.sIdle)
-  val memAddrReg     = RegInit(0.U(32.W))
-  val memWenReg      = RegInit(false.B)
-  val memReadDataReg = RegInit(0.U(32.W))
-  val reqValidReg    = RegInit(false.B)
-  val respReadyReg   = RegInit(true.B)
-  
-  val reqValidDelay  = Module(new RandomDelay(4))
-  val respReadyDelay = Module(new RandomDelay(4))
-  reqValidDelay.io.trigger  := false.B
-  respReadyDelay.io.trigger := false.B
+  val memRdataReg  = RegInit(0.U(32.W))
+  val memAddrReg   = RegInit(0.U(32.W))
+  val memWenReg    = RegInit(false.B)
+  val reqValidReg  = RegInit(false.B)
+  val respReadyReg = RegInit(true.B)
 
   val mem = Module(new MemExt())
   mem.io.clock     := clock
@@ -62,59 +57,42 @@ class LoadStoreUnit extends Module {
     is(State.sIdle) {
       when(io.in.valid && isLS) {
         when(mem.io.reqReady) {
-          //保留前三行：无随机延迟；后三行：加入随机延迟
           state        := State.sWait
-          reqValidReg  := true.B
-          respReadyReg := true.B
-          // state        := State.sDelay
-          // reqValidDelay.io.trigger  := true.B
-          // respReadyDelay.io.trigger := true.B
-
-
           memAddrReg   := io.in.bits.result
           memWenReg    := ctrl.memWen
-
+          reqValidReg  := true.B
+          respReadyReg := true.B
         }
 
-      }
-    }
-    is(State.sDelay) {
-      when(!reqValidReg) {
-        reqValidReg := reqValidDelay.io.ready
-      }
-      when(!respReadyReg) {
-        respReadyReg := respReadyDelay.io.ready
-      }
-      when(reqValidReg && respReadyReg) {
-        state := State.sWait
       }
     }
     // 等待状态:等待内存读取完成
     is(State.sWait) {
       reqValidReg := false.B
       when(mem.io.respValid) {
-        state     := State.sIdle
-        memReadDataReg := memReadData
-        memWenReg := false.B
+        state       := State.sFinish
+        memRdataReg := memReadData
+        memWenReg   := false.B
       }
     }
-
+    is(State.sFinish) {
+      state := State.sIdle
+    }
   }
 
   io.out.bits.ctrl     := ctrl
   io.out.bits.result   := io.in.bits.result
   io.out.bits.pc       := io.in.bits.pc
-  io.out.bits.memRdata := memReadDataReg
+  io.out.bits.memRdata := memRdataReg
   io.out.bits.imm      := io.in.bits.imm
   io.out.bits.csrRdata := io.in.bits.csrRdata
   io.out.bits.rd       := io.in.bits.rd
   io.out.bits.rdata1   := io.in.bits.rdata1
 
-  io.out.valid := io.in.valid && ((state === State.sIdle && !isLS) || (state === State.sWait && mem.io.respValid))
+  io.out.valid := io.in.valid && ((state === State.sIdle && !isLS) || (state === State.sFinish))
   io.in.ready  := state === State.sIdle
 
 }
-
 class MemExt extends ExtModule {
   val io = IO(new Bundle {
     val clock     = Input(Clock())
