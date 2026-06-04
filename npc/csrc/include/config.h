c9501147976f06e51f99d388755e74c8b89e3eb7
tracer-ysyx <tracer@ysyx.org>
2026-06-04 22:32:08 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:32:08 up  7:43,  1 user,  load average: 0.52, 0.40, 0.37

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#define ENABLE_DIFFTEST
+// #define ENABLE_DIFFTEST
 #define STEPS_AFTER_MISMATCH 10
 
 // #define ENABLE_ITRACE
 // #define ENABLE_FTRACE
 
 // 显示波形命令 gtkwave waveform.fst
-#define ENABLE_FST
+// #define ENABLE_FST
 #define MAX_SIM_TIME 1000
 #define FST_TRACE_LATEST false
 #define FST_START_TIME 0
