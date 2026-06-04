31e499287619b0f4449619272017833006b69809
tracer-ysyx <tracer@ysyx.org>
2026-06-05 00:57:51 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:57:51 up 10:09,  1 user,  load average: 0.16, 0.13, 0.15

--- npc/src/main/verilog/InstFetchUnit.v
+++ npc/src/main/verilog/InstFetchUnit.v
@@ -16,30 +16,31 @@ module InstFetchUnitExt (
     import "DPI-C" function int mem_read(input int addr);
 
 
-    wire resp_trigger, req_trigger,resp_delay_ready, req_delay_ready;
-    VRandomDelay #(.DELAY_BITS(4)) u_resp_delay (
-        .clock(io_clock),
-        .reset(io_reset),
-        .trigger(resp_trigger),
-        .ready(resp_delay_ready)
-    );
-    VRandomDelay #(.DELAY_BITS(4)) u_req_delay (
-        .clock(io_clock),
-        .reset(io_reset),
-        .trigger(req_trigger),
-        .ready(req_delay_ready)
-    );
+    // wire resp_trigger, req_trigger,resp_delay_ready, req_delay_ready;
+    // VRandomDelay #(.DELAY_BITS(4)) u_resp_delay (
+    //     .clock(io_clock),
+    //     .reset(io_reset),
+    //     .trigger(resp_trigger),
+    //     .ready(resp_delay_ready)
+    // );
+    // VRandomDelay #(.DELAY_BITS(4)) u_req_delay (
+    //     .clock(io_clock),
+    //     .reset(io_reset),
+    //     .trigger(req_trigger),
+    //     .ready(req_delay_ready)
+    // );
 
     parameter IDLE = 0, FETCH = 1, DELAY = 2;
     reg [1:0]state;
 
-    assign resp_trigger = (state == FETCH) && io_rready;
-    assign req_trigger  = (state == FETCH) && io_rready;
+    // assign resp_trigger = (state == FETCH) && io_rready;
+    // assign req_trigger  = (state == FETCH) && io_rready;
 
     always @(posedge io_clock)begin
         if(io_reset)begin
             state<=IDLE;
             io_arready<=1;
+            io_rvalid<=0;
         end
         else begin
             if (state==IDLE) begin
@@ -60,21 +61,21 @@ module InstFetchUnitExt (
                     end
 
             end
-            else if (state==DELAY) begin
-                if(!io_rvalid) begin
-                    if(resp_delay_ready) begin
-                        io_rvalid <= 1;
-                    end
-                end
-                if(!io_arready) begin
-                    if(req_delay_ready) begin
-                        io_arready <= 1;
-                    end
-                end
-                if (io_rvalid && io_arready) begin
-                    state <= IDLE;
-                end
-            end
+            // else if (state==DELAY) begin
+            //     if(!io_rvalid) begin
+            //         if(resp_delay_ready) begin
+            //             io_rvalid <= 1;
+            //         end
+            //     end
+            //     if(!io_arready) begin
+            //         if(req_delay_ready) begin
+            //             io_arready <= 1;
+            //         end
+            //     end
+            //     if (io_rvalid && io_arready) begin
+            //         state <= IDLE;
+            //     end
+            // end
         end
     end
     
