e6681f424570621409b0d0bdd94341ba14bec6f1
tracer-ysyx <tracer@ysyx.org>
2026-06-05 21:08:56 +0800

    > sim RTL mode:sim ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:08:56 up  8:06,  1 user,  load average: 0.15, 0.14, 0.14

--- npc/src/main/scala/bus/MemArbiter.scala
+++ npc/src/main/scala/bus/MemArbiter.scala
@@ -1,41 +1,59 @@
-// package top
-// import chisel3._
-// import chisel3.util._
+package top
+import chisel3._
+import chisel3.util._
 
-// class MemArbiter extends Module {
-//   val io = IO(new Bundle {
-//     val s0 = Flipped(new AXI4LiteIO)
-//     val s1 = Flipped(new AXI4LiteIO)
-//     val m  = new AXI4LiteIO
-//   })
+class MemArbiter extends Module {
+  val io = IO(new Bundle {
+    val s0 = Flipped(new AXI4LiteIO)
+    val s1 = Flipped(new AXI4LiteIO)
+    val m  = new AXI4LiteIO
+  })
+	
+	//默认阻断valid信号
+	io.m.arvalid := false.B
+	io.m.awvalid := false.B
+	io.m.wvalid  := false.B
+	
 
-//   object State extends ChiselEnum {
-//     val sIdle, sS0, sS1 = Value
-//   }
-//   object Conn  extends ChiselEnum {
-//     val s0, s1 = Value
-//   }
+  object State extends ChiselEnum {
+    val sIdle, sS0, sS1 = Value
+  }
+  object Conn  extends ChiselEnum {
+    val s0, s1 = Value
+  }
 
-//   val state    = RegInit(State.sIdle)
-//   val prevConn = RegInit(Conn.s0)
-//   val s0Valid  = s0.arvalid
-//   val s1Valid  = s1.arvalid || s1.awvalid || s1.wvalid
-
-
-//   switch(state) {
-//     is(State.sIdle) {
-//       when(s0Valid && s1Valid) {
-//         state := Mux(prevConn === Conn.s0, State.sS1, State.sS0)
-//       }.elsewhen(s0Valid) {
-//         state := State.sS0
-//       }.elsewhen(s1Valid) {
-//         state := State.sS1
-//       }
-//     }
-//     is(State.sS0) {
+  val state    = RegInit(State.sIdle)
+  val prevConn = RegInit(Conn.s0)
+  val s0Valid  = io.s0.arvalid
+  val s1Valid  = io.s1.arvalid || io.s1.awvalid || io.s1.wvalid
 
 
-// 		}
-//   }
+  switch(state) {
+    is(State.sIdle) {
+      when(s0Valid && s1Valid) {
+        state := Mux(prevConn === Conn.s0, State.sS1, State.sS0)
+      }.elsewhen(s0Valid) {
+        state := State.sS0
+      }.elsewhen(s1Valid) {
+        state := State.sS1
+      }
+    }
+    is(State.sS0) {
+			io.s0<>io.m
+			when(io.s0.rvalid && io.m.rready) {
+				state := State.sIdle
+				prevConn := Conn.s0
+			}
+		}
+		is(State.sS1) {
+			io.s1<>io.m
+			when((io.s1.rvalid && io.m.rready) || (io.s1.bvalid && io.m.bready)) {
+				state := State.sIdle
+				prevConn := Conn.s1
+			}
+		}
 
-// }
+		}
+  }
+
+}
