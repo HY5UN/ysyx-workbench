156fdc0f18aa090ee009dda26eab34c77d3821cc
tracer-ysyx <tracer@ysyx.org>
2026-06-05 14:19:06 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:19:06 up  1:16,  1 user,  load average: 1.07, 0.72, 0.51

--- npc/src/main/scala/1_IFU.scala
+++ npc/src/main/scala/1_IFU.scala
@@ -56,12 +56,8 @@ class InstFetchUnit extends Module {
   // 加入随机延迟
   val arvalidDelay = Module(new RandomDelay(4))
   val rreadyDelay  = Module(new RandomDelay(3))
-  val arvTrigger   = RegInit(false.B)
-  val rdyTrigger   = RegInit(false.B)
-  arvalidDelay.io.trigger := arvTrigger
-  rreadyDelay.io.trigger  := rdyTrigger
-  arvTrigger              := state === State.sIdle && io.in.valid
-  rdyTrigger              := ifuMem.io.arready && arvalidReg && state === State.sIdle
+  arvalidDelay.io.trigger := state === State.sIdle && io.in.valid
+  rreadyDelay.io.trigger  := ifuMem.io.arready && arvalidReg && state === State.sIdle
 
   switch(state) {
     is(State.sIdle) {
