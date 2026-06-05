d5aacc3bb8f7e8e7946f35b707884ba6e4882c0f
tracer-ysyx <tracer@ysyx.org>
2026-06-05 20:49:35 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:49:35 up  7:47,  1 user,  load average: 0.57, 0.27, 0.14

--- npc/src/main/scala/bus/MemArbiter.scala
+++ npc/src/main/scala/bus/MemArbiter.scala
@@ -1,41 +1,41 @@
-package top
-import chisel3._
-import chisel3.util._
+// package top
+// import chisel3._
+// import chisel3.util._
 
-class MemArbiter extends Module {
-  val io = IO(new Bundle {
-    val s0 = Flipped(new AXI4LiteIO)
-    val s1 = Flipped(new AXI4LiteIO)
-    val m  = new AXI4LiteIO
-  })
+// class MemArbiter extends Module {
+//   val io = IO(new Bundle {
+//     val s0 = Flipped(new AXI4LiteIO)
+//     val s1 = Flipped(new AXI4LiteIO)
+//     val m  = new AXI4LiteIO
+//   })
 
-  object State extends ChiselEnum {
-    val sIdle, sS0, sS1 = Value
-  }
-  object Conn  extends ChiselEnum {
-    val s0, s1 = Value
-  }
+//   object State extends ChiselEnum {
+//     val sIdle, sS0, sS1 = Value
+//   }
+//   object Conn  extends ChiselEnum {
+//     val s0, s1 = Value
+//   }
 
-  val state    = RegInit(State.sIdle)
-  val prevConn = RegInit(Conn.s0)
-  val s0Valid  = s0.arvalid
-  val s1Valid  = s1.arvalid || s1.awvalid || s1.wvalid
+//   val state    = RegInit(State.sIdle)
+//   val prevConn = RegInit(Conn.s0)
+//   val s0Valid  = s0.arvalid
+//   val s1Valid  = s1.arvalid || s1.awvalid || s1.wvalid
 
 
-  switch(state) {
-    is(State.sIdle) {
-      when(s0Valid && s1Valid) {
-        state := Mux(prevConn === Conn.s0, State.sS1, State.sS0)
-      }.elsewhen(s0Valid) {
-        state := State.sS0
-      }.elsewhen(s1Valid) {
-        state := State.sS1
-      }
-    }
-    is(State.sS0) {
-			
+//   switch(state) {
+//     is(State.sIdle) {
+//       when(s0Valid && s1Valid) {
+//         state := Mux(prevConn === Conn.s0, State.sS1, State.sS0)
+//       }.elsewhen(s0Valid) {
+//         state := State.sS0
+//       }.elsewhen(s1Valid) {
+//         state := State.sS1
+//       }
+//     }
+//     is(State.sS0) {
 
-		}
-  }
 
-}
+// 		}
+//   }
+
+// }
