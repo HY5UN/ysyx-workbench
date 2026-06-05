d1f424199b98e2da16a764849d5db78e152f3701
tracer-ysyx <tracer@ysyx.org>
2026-06-05 13:41:40 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:41:40 up 39 min,  1 user,  load average: 0.04, 0.10, 0.08

--- npc/src/main/scala/1_IFU.scala
+++ npc/src/main/scala/1_IFU.scala
@@ -12,18 +12,13 @@ class InstFetchUnit extends Module {
     val sIdle, sDelay, sWait = Value
   }
 
-  val araddrReg  = RegInit("h80000000".U(32.W))
-  val outInstReg   = RegInit(0.U(32.W))
-  val state      = RegInit(State.sWait)
-  val currPC     = RegInit("h80000000".U(32.W))
+  val araddrReg   = RegInit("h80000000".U(32.W))
+  val outInstReg  = RegInit(0.U(32.W))
+  val state       = RegInit(State.sWait)
+  val currPC      = RegInit("h80000000".U(32.W))
   val outValidReg = RegInit(false.B)
-  val arvalidReg = RegInit(true.B)
-  val rreadyReg  = RegInit(true.B)
-
-  val reqValidDelay  = Module(new RandomDelay(4))
-  val respReadyDelay = Module(new RandomDelay(3))
-  reqValidDelay.io.trigger  := false.B
-  respReadyDelay.io.trigger := false.B
+  val arvalidReg  = RegInit(true.B)
+  val rreadyReg   = RegInit(true.B)
 
   val ifuMem = Module(new InstFetchUnitExt())
 
@@ -33,44 +28,61 @@ class InstFetchUnit extends Module {
   ifuMem.io.clock   := clock
   ifuMem.io.reset   := reset
 
+  // switch(state) {
+  //   // 空闲状态:已取出指令,等待新的有效地址
+  //   is(State.sIdle) {
+  //     when(io.in.valid) {
+  //       araddrReg  := io.in.bits.nextPC
+  //       arvalidReg := true.B
+  //       outValidReg := false.B
+  //       when(ifuMem.io.arready) { // 地址通道握手成功
+  //         rreadyReg  := true.B
+  //         state      := State.sWait
+  //       }
+  //     }
+  //   }
+  //   // 等待状态:等待指令返回,准备输出
+  //   is(State.sWait) {
+  //     arvalidReg := false.B
+  //     when(ifuMem.io.rvalid){// 数据通道握手成功
+  //       state := State.sIdle
+  //       outInstReg:= ifuMem.io.rdata
+  //       outValidReg := true.B
+  //       rreadyReg  := false.B
+  //     }
+  //   }
+  // }
+
+  // 加入随机延迟
+  val arvalidDelay = Module(new RandomDelay(4))
+  val rreadyDelay  = Module(new RandomDelay(3))
+  arvalidDelay.io.trigger := state === State.sIdle && io.in.valid
+  rreadyDelay.io.trigger  := ifuMem.io.arready && arvalidReg
+
   switch(state) {
-    // 空闲状态:已取出指令,等待新的有效地址
     is(State.sIdle) {
       when(io.in.valid) {
-        araddrReg  := io.in.bits.nextPC
-        arvalidReg := true.B
+        araddrReg   := io.in.bits.nextPC
+        // arvalidReg := true.B
         outValidReg := false.B
-        when(ifuMem.io.arready) { // 地址通道握手成功
-          rreadyReg  := true.B
-          state      := State.sWait
+        when(!arvalidReg) { arvalidReg := arvalidDelay.io.ready }
+        when(!rreadyReg) { rreadyReg := rreadyDelay.io.ready }
+        when(rreadyReg) {
+          state := State.sWait
         }
-
       }
     }
-    is(State.sDelay) {
-      when(!arvalidReg) {
-        arvalidReg := reqValidDelay.io.ready
-      }
-      when(!rreadyReg) {
-        rreadyReg := respReadyDelay.io.ready
-      }
-      when(arvalidReg && rreadyReg) {
-        state := State.sWait
-      }
-    }
-    // 等待状态:等待指令返回,准备输出
     is(State.sWait) {
       arvalidReg := false.B
-      when(ifuMem.io.rvalid){// 数据通道握手成功
-        state := State.sIdle
-        outInstReg:= ifuMem.io.rdata
+      when(ifuMem.io.rvalid) { // 数据通道握手成功
+        state       := State.sIdle
+        outInstReg  := ifuMem.io.rdata
         outValidReg := true.B
-        rreadyReg  := false.B
+        rreadyReg   := false.B
       }
-
     }
-
   }
+
   io.out.valid := outValidReg
   io.in.ready  := state === State.sIdle
 
@@ -84,12 +96,12 @@ class InstFetchUnitExt extends ExtModule {
     val clock = Input(Clock())
     val reset = Input(Bool())
 
-    val araddr = Input(UInt(32.W))
+    val araddr  = Input(UInt(32.W))
     val arvalid = Input(Bool())
     val arready = Output(Bool())
 
-    val rdata = Output(UInt(32.W))
-    val rresp = Output(UInt(2.W))
+    val rdata  = Output(UInt(32.W))
+    val rresp  = Output(UInt(2.W))
     val rvalid = Output(Bool())
     val rready = Input(Bool())
 
