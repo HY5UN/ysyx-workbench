a3a50c6f4c0d1dff855ba13728638688d25c396a
tracer-ysyx <tracer@ysyx.org>
2026-06-05 17:49:45 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:49:45 up  4:47,  1 user,  load average: 0.47, 0.57, 0.37

--- npc/src/main/scala/utils/randomDelay.scala
+++ npc/src/main/scala/utils/randomDelay.scala
@@ -1,37 +1,116 @@
 package top
+
 import chisel3._
 import chisel3.util._
-import chisel3.util.random.LFSR
 
-
-class RandomDelay(maxBits: Int = 4) extends Module {
+class InstFetchUnitExt extends ExtModule {
   val io = IO(new Bundle {
-    val trigger = Input(Bool())
-    val ready   = Output(Bool())
+
+    val clock = Input(Clock())
+    val reset = Input(Bool())
+
+    val axi = Flipped(new AXI4LiteIO)
+
   })
+}
 
-  val triggerPrev = RegNext(io.trigger, false.B)
-  val risingEdge  = io.trigger && !triggerPrev
-
-  val lfsr     = LFSR(8, risingEdge)        // 仅上升沿时采样新随机值
-  val cnt      = RegInit(0.U(maxBits.W))
-  val active   = RegInit(false.B)
-  val readyReg = RegInit(false.B)
-  readyReg := false.B
-
-  when(risingEdge && !active) { //仅在trigger上升沿激活
-    cnt    := lfsr(maxBits - 1, 0)
-    active := true.B
+class InstFetchUnit extends Module {
+  val io = IO(new Bundle {
+    val out = Decoupled(new IFU2IDU)
+    val in  = Flipped(Decoupled(new WBU2IFU))
+  })
+  object State extends ChiselEnum {
+    val sIdle, sDelay, sWait = Value
   }
 
-  when(active) {
-    when(cnt === 0.U) {
-      readyReg := true.B
-      active   := false.B
-    }.otherwise {
-      cnt := cnt - 1.U
+  val araddrReg   = RegInit("h80000000".U(32.W))
+  val outInstReg  = RegInit(0.U(32.W))
+  val state       = RegInit(State.sWait)
+  val currPC      = RegInit("h80000000".U(32.W))
+  val outValidReg = RegInit(false.B)
+  val arvalidReg  = RegInit(true.B)
+  val rreadyReg   = RegInit(true.B)
+
+  val ifuMem = Module(new InstFetchUnitExt())
+
+  ifuMem.io.axi.araddr  := araddrReg
+  ifuMem.io.axi.arvalid := arvalidReg
+  ifuMem.io.axi.rready  := rreadyReg
+  ifuMem.io.clock       := clock
+  ifuMem.io.reset       := reset
+
+  // 写通道不使用，接 0
+  ifuMem.io.axi.awaddr  := 0.U
+  ifuMem.io.axi.awvalid := false.B
+  ifuMem.io.axi.wdata   := 0.U
+  ifuMem.io.axi.wstrb   := 0.U
+  ifuMem.io.axi.wvalid  := false.B
+  ifuMem.io.axi.bready  := false.B
+
+  // switch(state) {
+  //   // 空闲状态:已取出指令,等待新的有效地址
+  //   is(State.sIdle) {
+  //     when(io.in.valid) {
+  //       araddrReg  := io.in.bits.nextPC
+  //       arvalidReg := true.B
+  //       outValidReg := false.B
+  //       when(ifuMem.io.arready) { // 地址通道握手成功
+  //         rreadyReg  := true.B
+  //         state      := State.sWait
+  //       }
+  //     }
+  //   }
+  //   // 等待状态:等待指令返回,准备输出
+  //   is(State.sWait) {
+  //     arvalidReg := false.B
+  //     when(ifuMem.io.rvalid){// 数据通道握手成功
+  //       state := State.sIdle
+  //       outInstReg:= ifuMem.io.rdata
+  //       outValidReg := true.B
+  //       rreadyReg  := false.B
+  //     }
+  //   }
+  // }
+
+  // 加入随机延迟
+  val arvalidDelay = Module(new RandomDelay(4))
+  val rreadyDelay  = Module(new RandomDelay(3))
+  arvalidDelay.io.trigger := false.B
+  rreadyDelay.io.trigger  := false.B
+
+  switch(state) {
+    is(State.sIdle) {
+      when(io.in.valid) {
+        araddrReg               := io.in.bits.nextPC
+        outValidReg             := false.B
+        arvalidDelay.io.trigger := true.B
+      }
+      arvalidReg := arvalidDelay.io.ready || arvalidReg
+      when(ifuMem.io.axi.arready && arvalidReg){//读请求握手
+        rreadyDelay.io.trigger := true.B
+      }
+
+      when(!rreadyReg) {
+        when(rreadyDelay.io.ready) {
+          rreadyReg := true.B
+          state     := State.sWait
+        }
+      }
+    }
+    is(State.sWait) {
+      arvalidReg := false.B
+      when(ifuMem.io.axi.rvalid) { // 读响应握手
+        state       := State.sIdle
+        outInstReg  := ifuMem.io.axi.rdata
+        outValidReg := true.B
+        rreadyReg   := false.B
+      }
     }
   }
 
-  io.ready := readyReg
+  io.out.valid := outValidReg
+  io.in.ready  := state === State.sIdle
+
+  io.out.bits.inst := outInstReg
+  io.out.bits.pc   := araddrReg
 }
