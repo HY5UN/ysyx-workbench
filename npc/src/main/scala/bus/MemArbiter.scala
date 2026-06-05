ca670b68d8e8d6968defcdb03a484865560d9c65
tracer-ysyx <tracer@ysyx.org>
2026-06-05 22:42:37 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:42:37 up  9:40,  1 user,  load average: 0.27, 0.40, 0.46

--- npc/src/main/scala/bus/MemArbiter.scala
+++ npc/src/main/scala/bus/MemArbiter.scala
@@ -17,12 +17,9 @@ class MemArbiter extends Module {
   object State extends ChiselEnum {
     val sIdle, sS0, sS1 = Value
   }
-  object Conn  extends ChiselEnum {
-    val s0, s1 = Value
-  }
+
 
   val state    = RegInit(State.sS0)
-  val prevConn = RegInit(Conn.s0)
   val s0Valid  = io.s0.arvalid
   val s1Valid  = io.s1.arvalid || io.s1.awvalid || io.s1.wvalid
 
@@ -30,20 +27,9 @@ class MemArbiter extends Module {
   val s1Finish = RegInit(false.B)
 
   switch(state) {
-    // is(State.sIdle) {
-    //   when(s0Valid && s1Valid) {
-    //     state := Mux(prevConn === Conn.s0, State.sS1, State.sS0)
-    //   }.elsewhen(s0Valid) {
-    //     state := State.sS0
-    //   }.elsewhen(s1Valid) {
-    //     state := State.sS1
-    //   }
-    // }
     is(State.sS0) {
       io.s0 <> io.m
       when(io.s0.rvalid && io.m.rready) {
-        // state    := State.sIdle
-        // prevConn := Conn.s0
         s0Finish := true.B
       }
       when(s0Finish) {
@@ -57,8 +43,6 @@ class MemArbiter extends Module {
     is(State.sS1) {
       io.s1 <> io.m
       when((io.s1.rvalid && io.m.rready) || (io.s1.bvalid && io.m.bready)) {
-        // state    := State.sIdle
-        // prevConn := Conn.s1
 				s1Finish := true.B
       }
 			when(s1Finish) {
