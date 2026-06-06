f991560a2f9329c7a3e3bebaf232bdc3ab00b71e
tracer-ysyx <tracer@ysyx.org>
2026-06-06 12:00:45 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:00:45 up  2:15,  1 user,  load average: 0.34, 0.24, 0.36

--- npc/src/main/scala/bus/UART.scala
+++ npc/src/main/scala/bus/UART.scala
@@ -22,6 +22,8 @@ class UART extends Module {
   io.axi.wready  := wreadyReg
   io.axi.bvalid  := bvalidReg
 
+  val writer = Module(new WriteChar)
+
   switch(state) {
     is(State.sIdle) {
       bvalidReg := false.B
@@ -34,7 +36,7 @@ class UART extends Module {
     is(State.sBusy) {
       // printf("%c", io.axi.wdata(7, 0))
       writer.io.data := io.axi.wdata(7, 0)
-      bvalidReg := true.B
+      bvalidReg      := true.B
       when(io.axi.bready) {
         state      := State.sIdle
         awreadyReg := true.B
@@ -49,11 +51,13 @@ class WriteChar extends BlackBox {
   val io = IO(new Bundle {
     val data = Input(UInt(8.W))
   })
-  setInline("WriteChar.v",
-    s"""module WriteChar(
-       |  input [7:0] data
-       |);
-       |  always @(*) $$write("%c", data);
-       |endmodule
-     """.stripMargin)
+  setInline(
+    "WriteChar.v",
+    """module WriteChar(
+      |  input [7:0] data
+      |);
+      |  always @(*) $write("%c", data);
+      |endmodule
+    """.stripMargin
+  )
 }
