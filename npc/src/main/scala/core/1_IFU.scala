ca275be7a8698183777e2c91fd271fe6f8f0b073
tracer-ysyx <tracer@ysyx.org>
2026-06-07 13:41:13 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:41:13 up 8 min,  1 user,  load average: 0.27, 0.38, 0.24

--- npc/src/main/scala/core/1_IFU.scala
+++ npc/src/main/scala/core/1_IFU.scala
@@ -13,6 +13,10 @@ class InstFetchUnit extends Module {
     val sIdle, sDelay, sWait = Value
   }
 
+  val tie0 = new AXI4LiteTie0
+  tie0.io.s <> tie0.io.m
+  tie0.io.s <> io.memIO
+
   val araddrReg   = RegInit("h80000000".U(32.W))
   val outInstReg  = RegInit(0.U(32.W))
   val state       = RegInit(State.sIdle)
@@ -25,14 +29,6 @@ class InstFetchUnit extends Module {
   io.memIO.arvalid := arvalidReg
   io.memIO.rready  := rreadyReg
 
-  // 写通道不使用，接 0
-  io.memIO.awaddr  := 0.U
-  io.memIO.awvalid := false.B
-  io.memIO.wdata   := 0.U
-  io.memIO.wstrb   := 0.U
-  io.memIO.wvalid  := false.B
-  io.memIO.bready  := false.B
-
   switch(state) {
     // 空闲状态:已取出指令,等待新的有效地址
     is(State.sIdle) {
@@ -41,7 +37,7 @@ class InstFetchUnit extends Module {
         arvalidReg  := true.B
         outValidReg := false.B
       }
-      when(io.memIO.arready&& (arvalidReg||io.in.valid)) { // 地址通道握手成功
+      when(io.memIO.arready && (arvalidReg || io.in.valid)) { // 地址通道握手成功
         rreadyReg := true.B
         state     := State.sWait
       }
@@ -49,7 +45,7 @@ class InstFetchUnit extends Module {
     // 等待状态:等待指令返回,准备输出
     is(State.sWait) {
       arvalidReg := false.B
-      when(io.memIO.rvalid&&rreadyReg) { // 数据通道握手成功
+      when(io.memIO.rvalid && rreadyReg) { // 数据通道握手成功
         state       := State.sIdle
         outInstReg  := io.memIO.rdata
         outValidReg := true.B
@@ -58,8 +54,6 @@ class InstFetchUnit extends Module {
     }
   }
 
- 
-
   io.out.valid := outValidReg
   io.in.ready  := state === State.sIdle
 
@@ -67,39 +61,38 @@ class InstFetchUnit extends Module {
   io.out.bits.pc   := araddrReg
 }
 
+// 加入随机延迟
+// val arvalidDelay = Module(new RandomDelay(4))
+// val rreadyDelay  = Module(new RandomDelay(3))
+// arvalidDelay.io.trigger := false.B
+// rreadyDelay.io.trigger  := false.B
 
- // 加入随机延迟
-  // val arvalidDelay = Module(new RandomDelay(4))
-  // val rreadyDelay  = Module(new RandomDelay(3))
-  // arvalidDelay.io.trigger := false.B
-  // rreadyDelay.io.trigger  := false.B
+// switch(state) {
+//   is(State.sIdle) {
+//     when(io.in.valid) {
+//       araddrReg               := io.in.bits.nextPC
+//       outValidReg             := false.B
+//       arvalidDelay.io.trigger := true.B
+//     }
+//     arvalidReg := arvalidDelay.io.ready || arvalidReg
+//     when(io.memIO.arready && arvalidReg){//读请求握手
+//       rreadyDelay.io.trigger := true.B
+//     }
 
-  // switch(state) {
-  //   is(State.sIdle) {
-  //     when(io.in.valid) {
-  //       araddrReg               := io.in.bits.nextPC
-  //       outValidReg             := false.B
-  //       arvalidDelay.io.trigger := true.B
-  //     }
-  //     arvalidReg := arvalidDelay.io.ready || arvalidReg
-  //     when(io.memIO.arready && arvalidReg){//读请求握手
-  //       rreadyDelay.io.trigger := true.B
-  //     }
-
-  //     when(!rreadyReg) {
-  //       when(rreadyDelay.io.ready) {
-  //         rreadyReg := true.B
-  //         state     := State.sWait
-  //       }
-  //     }
-  //   }
-  //   is(State.sWait) {
-  //     arvalidReg := false.B
-  //     when(io.memIO.rvalid) { // 读响应握手
-  //       state       := State.sIdle
-  //       outInstReg  := io.memIO.rdata
-  //       outValidReg := true.B
-  //       rreadyReg   := false.B
-  //     }
-  //   }
-  // }
+//     when(!rreadyReg) {
+//       when(rreadyDelay.io.ready) {
+//         rreadyReg := true.B
+//         state     := State.sWait
+//       }
+//     }
+//   }
+//   is(State.sWait) {
+//     arvalidReg := false.B
+//     when(io.memIO.rvalid) { // 读响应握手
+//       state       := State.sIdle
+//       outInstReg  := io.memIO.rdata
+//       outValidReg := true.B
+//       rreadyReg   := false.B
+//     }
+//   }
+// }
