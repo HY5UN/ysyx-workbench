f14308116577f636ddc7d03043c3b8d4438b866e
tracer-ysyx <tracer@ysyx.org>
2026-06-09 09:24:00 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:24:00 up 39 min,  1 user,  load average: 0.03, 0.05, 0.07

--- npc/csrc/memory/rom.cpp
+++ npc/csrc/memory/rom.cpp
@@ -8,29 +8,6 @@ uint32_t rom_begin = 0x20000000;
 uint32_t rom_end = 0x20000fff;
 uint32_t rom[4096 / 4];
 
-void init_rom(const std::string &path)
-{
-  load_rom(path);
-
-}
-
-extern "C" void mrom_read(int32_t addr, int32_t *data)
-{
-    
-    addr &= 0xfffffffc;
-    if (addr >= rom_begin && addr < rom_end)
-    {
-        *data = rom[(addr - rom_begin) / 4];
-    }
-    else
-    {
-        std::cerr << "mrom_read: Address out of range: " << std::hex << addr << std::dec << std::endl;
-        *data = 0; // 或者其他默认值
-    }
-
-    printf("mrom_read: addr=0x%08x, data=0x%08x\n", addr, *data);
-}
-
 void load_rom(const std::string &path)
 {
     std::ifstream f(path, std::ios::binary | std::ios::ate);
@@ -55,3 +32,27 @@ void load_rom(const std::string &path)
 
     std::cout << "load_rom: loaded " << size << " bytes from " << path << std::endl;
 }
+
+void init_rom(const std::string &path)
+{
+  load_rom(path);
+
+}
+
+extern "C" void mrom_read(int32_t addr, int32_t *data)
+{
+    
+    addr &= 0xfffffffc;
+    if (addr >= rom_begin && addr < rom_end)
+    {
+        *data = rom[(addr - rom_begin) / 4];
+    }
+    else
+    {
+        std::cerr << "mrom_read: Address out of range: " << std::hex << addr << std::dec << std::endl;
+        *data = 0; // 或者其他默认值
+    }
+
+    printf("mrom_read: addr=0x%08x, data=0x%08x\n", addr, *data);
+}
+
