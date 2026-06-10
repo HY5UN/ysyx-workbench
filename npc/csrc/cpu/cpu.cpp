ca157750e60b9ed933925b90ad4ac86a61fc799e
tracer-ysyx <tracer@ysyx.org>
2026-06-10 20:49:17 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:49:17 up  6:18,  1 user,  load average: 0.23, 0.48, 0.37

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -103,6 +103,8 @@ bool CPU::execute_once()
 
     contextp->timeInc(1);
 
+    printf("\rCycle: %lld, PC: 0x%08x", cycle_count, dut_CPU_state.pc);
+
     if (dpic_ebreak_triggered)
     {
         difftest->in_mismatch = false; 
