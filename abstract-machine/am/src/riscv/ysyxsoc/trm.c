26c86d587a5e64a168dae6d085800b062326dee6
tracer-ysyx <tracer@ysyx.org>
2026-06-11 18:23:36 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:23:36 up 1 day,  4:15,  1 user,  load average: 0.43, 0.60, 0.50

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -47,11 +47,12 @@ void bootloader(){
 #define UART_LCR   (UART_BASE + 0x03)
 #define UART_LSR   (UART_BASE + 0x05)
 void init_uart(){
+  outb(UART_FCR,0b00000111);
   outb(UART_LCR,0b10000011);
   outb(UART_DLM,0x1);
   outb(UART_DLL,0x0);
   outb(UART_LCR,0b00000011);
-  outb(UART_FCR,0b11000111);
+  outb(UART_FCR,0b11000001);
 
 }
 void putch(char ch) {
