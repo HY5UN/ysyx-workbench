e22f1d9d82ecd2877615b7187ecc10d39bec94ce
tracer-ysyx <tracer@ysyx.org>
2026-06-11 19:56:52 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:56:52 up 1 day,  5:51,  1 user,  load average: 1.22, 0.95, 0.60

--- npc/src/main/scala/bus/mockSoc/ysyxSoCFull.scala
+++ npc/src/main/scala/bus/mockSoc/ysyxSoCFull.scala
@@ -6,6 +6,12 @@ class ysyxSoCFull extends Module {
   val io = IO(new Bundle {})
 
   val core = Module(new ysyx_26010036)
+  core.io.interrupt := 0.U
+  val tie0m = Module(new AXI4MasterTie0)
+  val tie0s = Module(new AXI4SlaveTie0)
+    tie0s.io.s <> core.io.master  
+    core.io.master <> tie0m.io.m
+
 
   val mem   = Module(new MemExt)
   val uart  = Module(new UART)
