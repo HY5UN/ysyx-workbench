38c4105d11559cc426b6e65ca82b504bb47e11c5
tracer-ysyx <tracer@ysyx.org>
2026-06-17 17:43:15 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:43:15 up  7:18,  1 user,  load average: 1.96, 0.91, 0.57

--- npc/csrc/memory/psram.cpp
+++ npc/csrc/memory/psram.cpp
@@ -42,7 +42,7 @@ void init_psram(const std::string &path)
 
 extern "C" void psram_read(int addr, char *rdata)
 {
-    // printf("psram_read: addr=0x%08x\n", addr);
+    printf("psram_read: addr=0x%08x\n", addr);
     if ((uint32_t)addr >= PSRAM_SIZE) {
         fprintf(stderr, "psram_read: addr 0x%x out of range\n", addr);
         *rdata = 0;
@@ -53,7 +53,7 @@ extern "C" void psram_read(int addr, char *rdata)
 
 extern "C" void psram_write(int addr, char wdata)
 {
-    // printf("psram_write: addr=0x%08x, data=0x%02x\n", addr, (uint8_t)wdata);
+    printf("psram_write: addr=0x%08x, data=0x%02x\n", addr, (uint8_t)wdata);
     if ((uint32_t)addr >= PSRAM_SIZE) {
         fprintf(stderr, "psram_write: addr 0x%x out of range\n", addr);
         return;
