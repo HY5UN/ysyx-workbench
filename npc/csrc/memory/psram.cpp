d2f8982881a67f462429b9b2d979ed4f3070843e
tracer-ysyx <tracer@ysyx.org>
2026-06-18 17:02:15 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:02:15 up 18:53,  1 user,  load average: 0.33, 0.34, 0.28

--- npc/csrc/memory/psram.cpp
+++ npc/csrc/memory/psram.cpp
@@ -43,12 +43,14 @@ void init_psram(const std::string &path)
 
 extern "C" void psram_read(int addr, char *rdata)
 {
+
     if ((uint32_t)addr >= PSRAM_SIZE) {
         fprintf(stderr, "psram_read: addr 0x%x out of range\n", addr);
         *rdata = 0;
         return;
     }
     *rdata = (char)psram[addr];
+    printf(".")
     // printf("[NPC] psram_read: addr=0x%08x, data=0x%02x, at cycle=%llu\n", addr, (uint8_t)*rdata, cpu->cycle_count);
 }
 
@@ -59,5 +61,6 @@ extern "C" void psram_write(int addr, char wdata)
         return;
     }
     psram[addr] = (uint8_t)wdata;
+    printf(".");
     // printf("[NPC] psram_write: addr=0x%08x, data=0x%02x, at cycle=%llu\n", addr, (uint8_t)wdata, cpu->cycle_count);
 }
