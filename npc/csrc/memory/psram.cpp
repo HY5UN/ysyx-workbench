fd6ae52d77bc72d115477e5e7c99e04956545ec6
tracer-ysyx <tracer@ysyx.org>
2026-06-18 17:06:41 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:06:41 up 18:57,  1 user,  load average: 0.15, 0.25, 0.26

--- npc/csrc/memory/psram.cpp
+++ npc/csrc/memory/psram.cpp
@@ -50,7 +50,7 @@ extern "C" void psram_read(int addr, char *rdata)
         return;
     }
     *rdata = (char)psram[addr];
-    printf(".");
+    // printf(".");
     // printf("[NPC] psram_read: addr=0x%08x, data=0x%02x, at cycle=%llu\n", addr, (uint8_t)*rdata, cpu->cycle_count);
 }
 
@@ -61,6 +61,6 @@ extern "C" void psram_write(int addr, char wdata)
         return;
     }
     psram[addr] = (uint8_t)wdata;
-    printf(".");
+    // printf(".");
     // printf("[NPC] psram_write: addr=0x%08x, data=0x%02x, at cycle=%llu\n", addr, (uint8_t)wdata, cpu->cycle_count);
 }
