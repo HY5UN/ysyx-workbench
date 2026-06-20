48e337ac69d5b7fdb3ea31eead07da3a761ca10d
tracer-ysyx <tracer@ysyx.org>
2026-06-20 13:09:14 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:09:14 up 20:35,  1 user,  load average: 0.31, 0.35, 0.35

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -3,9 +3,10 @@
 // #define ENABLE_DIFFTEST
 #define STEPS_AFTER_MISMATCH 10
 
-#define ENABLE_ITRACE
+// #define ENABLE_ITRACE
 #define MTRACE_ONLY
 #define ITRACE_MAX_LINES 5000
+
 // #define ENABLE_FTRACE
 #define FTRACE_MAX_LINES 5000
 
