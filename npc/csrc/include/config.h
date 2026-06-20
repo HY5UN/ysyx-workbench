57537bc694bd750c722e6648fe26bfa3115074e2
tracer-ysyx <tracer@ysyx.org>
2026-06-20 14:31:59 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:31:59 up 22:00,  1 user,  load average: 1.09, 0.68, 0.59

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -3,14 +3,14 @@
 // #define ENABLE_DIFFTEST
 #define STEPS_AFTER_MISMATCH 10
 
-#define ENABLE_ITRACE
+// #define ENABLE_ITRACE
 #define MTRACE_ONLY
 #define ITRACE_MAX_LINES 5000
 
 // #define ENABLE_FTRACE
 #define FTRACE_MAX_LINES 5000
 
-#define ENABLE_FST // 查看波形命令 gtkwave waveform.fst
+// #define ENABLE_FST // 查看波形命令 gtkwave waveform.fst
 #define MAX_SIM_TIME 5000
 #define FST_TRACE_LATEST false
 #define FST_START_TIME 2471000 //如果记录时钟下降沿，需要乘以2
