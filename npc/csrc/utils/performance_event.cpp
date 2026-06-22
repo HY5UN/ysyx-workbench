98026fd78ee671ac55c896761883396661e54003
tracer-ysyx <tracer@ysyx.org>
2026-06-22 20:38:53 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:38:53 up  9:25,  1 user,  load average: 0.52, 0.63, 0.69

--- npc/csrc/utils/performance_event.cpp
+++ npc/csrc/utils/performance_event.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "include/common.h"
+#include "include/CPU.h"
 // ==========================================
 // 全局状态与计数器定义
 // ==========================================
@@ -120,6 +121,9 @@ extern "C" void dpic_save_performance_event(
     if (io_lsu_r_finish) {
         total_lsur_cycles += current_lsur_counter;
         total_lsur_counts++;
+        if(current_lsur_counter > 10){
+            printf("[Warning] Detected a long LSU read latency: %lu cycles, cycle: %lu\n", current_lsur_counter, cpu->cycle_count);
+        }
         is_lsur = false;
     }
 
