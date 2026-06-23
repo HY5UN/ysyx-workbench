612162f51cf65de14ba6a31363942a3cb629da22
tracer-ysyx <tracer@ysyx.org>
2026-06-23 17:52:21 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:52:21 up 18:38,  1 user,  load average: 0.33, 0.61, 0.59

---
+++ npc/src/main/scala/bus/mockSoc/MemExt.scala
@@ -0,0 +1,11 @@
+package top
+import chisel3._
+import chisel3.util._
+
+class MemExt extends ExtModule {
+  val io = IO(new Bundle {
+    val clock = Input(Clock())
+    val reset = Input(Bool())
+    val axi   = Flipped(new AXI4IO)
+  })
+}
