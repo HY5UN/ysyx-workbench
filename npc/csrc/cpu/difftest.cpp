b3421a8c8f398a9d61b57bcd166b78443b3aff17
tracer-ysyx <tracer@ysyx.org>
2026-06-25 23:27:00 +0800

    > compile NEMU ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:27:00 up 12:27,  1 user,  load average: 0.48, 0.25, 0.19

--- npc/csrc/cpu/difftest.cpp
+++ npc/csrc/cpu/difftest.cpp
@@ -101,11 +101,3 @@ void dpic_save_gprs(
     dut_CPU_state.gpr[15] = (word_t)gpr15;
 }
 
-// void DiffTest::gen_pc_seq(){
-//     while(true){
-//         difftest_exec(1);
-//         difftest_regcpy(&ref_CPU_state, DIFFTEST_TO_DUT);
-//     }
-    
-
-// }
