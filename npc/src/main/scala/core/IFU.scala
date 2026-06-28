6e1f058f5619aa5d5d28839cfa474cc6aac6f584
tracer-ysyx <tracer@ysyx.org>
2026-06-28 16:53:15 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:53:15 up 2 days, 21:49,  1 user,  load average: 0.95, 0.74, 0.48

--- npc/src/main/scala/core/IFU.scala
+++ npc/src/main/scala/core/IFU.scala
@@ -30,7 +30,7 @@ class IFU extends Module {
     val sInit, sIdle, sPcWait, sIWait, sOut = Value
   }
   val state = RegInit(State.sInit)
-  val icache = Module(new ICache(cacheSizeB = 32, blockSizeB = 16, assoc = 1))
+  val icache = Module(new ICache(cacheSizeB = 32, blockSizeB = 4, assoc = 1))
   icache.io.axi <> io.axi
   icache.io.ifu.pc        := araddrReg
   icache.io.ifu.pcValid   := state === State.sPcWait
