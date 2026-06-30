4ecd98dd37678d8ed83f8eba5363777c7dc970b4
tracer-ysyx <tracer@ysyx.org>
2026-06-30 19:12:33 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:12:33 up 1 day,  7:18,  1 user,  load average: 0.53, 0.63, 0.50

--- npc/csrc/utils/npc_putch.cpp
+++ npc/csrc/utils/npc_putch.cpp
@@ -2,10 +2,10 @@
 #include "include/CPU.h"
 void dpic_putch(char c)
 {
+    putchar(c);
     if(c == '\n')
     {
-        printf ("       @pc: 0x%08x, cycle: %llu, ×2=%llu", cpu->pc, (unsigned long long)cpu->cycle_count,(unsigned long long)cpu->cycle_count*2);
+        printf ("[pc: 0x%08x, cycle: %llu, ×2=%llu]     ", cpu->pc, (unsigned long long)cpu->cycle_count,(unsigned long long)cpu->cycle_count*2);
     }
-    putchar(c);
 
 }
