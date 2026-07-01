3e7b6f67e8e60e8ba2abe05c717aa604d8b3fa09
tracer-ysyx <tracer@ysyx.org>
2026-07-01 13:33:13 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:33:13 up 1 day, 16:15,  1 user,  load average: 0.52, 0.41, 0.26

--- npc/src/main/scala/bus/mockSoc/MemXbar.scala
+++ npc/src/main/scala/bus/mockSoc/MemXbar.scala
@@ -4,69 +4,123 @@ import chisel3.util._
 
 class MemXbar extends Module {
   val io = IO(new Bundle {
-    val s     = Flipped(new AXI4IO)
-    val mUART = new AXI4IO
-    val mRAM  = new AXI4IO
+    val s      = Flipped(new AXI4IO)
+    val mUART  = new AXI4IO
+    val mRAM   = new AXI4IO
     val mCLINT = new AXI4IO
   })
+  ChiselUtils.driveZeroOutputs(io)
 
-  val tie0m = Module(new AXI4MasterTie0)
-  val tie0s = Module(new AXI4SlaveTie0)
-  tie0s.io.s <> io.s
-  tie0m.io.m <> io.mUART
-  tie0m.io.m <> io.mRAM
-  tie0m.io.m <> io.mCLINT
+  val UART  = AddressSpace(0x10000000L, 0x4L)
+  val RAM   = AddressSpace(0x80000000L, 1024 * 1024 * 64 * 8L)
+  val CLINT = AddressSpace(0x10000028L, 0x4L)
 
-  val UART = AddressSpace(0x10000000L, 0x4L)
-  val RAM  = AddressSpace(0x80000000L, 1024 * 1024 * 64 * 8L)
-  val CLINT = AddressSpace(0x10000028L,0x4L)
+  def connectW(m: Data, s: Data) = {
+    m.awready := s.awready
+    s.awvalid := m.awvalid
+    s.awaddr  := m.awaddr
+    s.awid    := m.awid
+    s.awlen   := m.awlen
+    s.awsize  := m.awsize
+    s.awburst := m.awburst
+
+    m.wready := s.wready
+    s.wvalid := m.wvalid
+    s.wdata  := m.wdata
+    s.wstrb  := m.wstrb
+    s.wlast  := m.wlast
+
+    s.bready := m.bready
+    m.bvalid := s.bvalid
+    m.bresp  := s.bresp
+    m.bid    := s.bid
+  }
+
+  def connectR(m: Data, s: Data) = {
+    m.arready := s.arready
+    s.arvalid := m.arvalid
+    s.araddr  := m.araddr
+    s.arid    := m.arid
+    s.arlen   := m.arlen
+    s.arsize  := m.arsize
+    s.arburst := m.arburst
+
+    s.rready := m.rready
+    m.rvalid := s.rvalid
+    m.rdata  := s.rdata
+    m.rresp  := s.rresp
+    m.rlast  := s.rlast
+    m.rid    := s.rid
+  }
 
   object State extends ChiselEnum {
-    val sIdle, sMUART, sMRAM, sCLINT = Value
+    val sIdle, sUART, sRAM, sCLINT = Value
   }
   val state = RegInit(State.sIdle)
 
-  
-  val isReadReg = RegInit(false.B)
-  val validReg = RegInit(false.B)
-  isReadReg := io.s.arvalid
-  validReg  := io.s.arvalid || io.s.awvalid || io.s.wvalid
-  val addr   = Mux(isReadReg, io.s.araddr, io.s.awaddr)
-
-  switch(state) {
+  val wstate = RegInit(State.sIdle)
+  switch(wstate) {
     is(State.sIdle) {
-      when(validReg) {
-        when(UART.contains(addr)) {
-          state := State.sMUART
+      when(io.s.awvalid) {
+        when(UART.contains(io.s.awaddr)) {
+          wstate := State.sUART
+        }.elsewhen(CLINT.contains(io.s.awaddr)) {
+          wstate := State.sCLINT
+        }.otherwise {
+          wstate := State.sRAM
+
         }
-          // .elsewhen(RAM.contains(addr)) {
-          //   state := State.sMRAM
-          // }
-          .elsewhen(CLINT.contains(addr)) {
-            state := State.sCLINT
-          }
-          .otherwise {
-            state := State.sMRAM
-          }
-          validReg := false.B
       }
     }
-    is(State.sMUART) {
-      io.mUART <> io.s
-      when((io.s.rvalid && io.mUART.rready) || (io.s.bvalid && io.mUART.bready)) {
-        state := State.sIdle
-      }
-    }
-    is(State.sMRAM) {
-      io.mRAM <> io.s
-      when((io.s.rvalid && io.mRAM.rready && io.mRAM.rlast) || (io.s.bvalid && io.mRAM.bready)) {
-        state := State.sIdle
+    is(State.sUART) {
+      connectW(io.s, io.mUART)
+      when(io.s.bready && io.mUART.bvalid) {
+        wstate := State.sIdle
       }
     }
     is(State.sCLINT) {
-      io.mCLINT <> io.s
-      when((io.s.rvalid && io.mCLINT.rready) || (io.s.bvalid && io.mCLINT.bready)) {
-        state := State.sIdle
+      connectW(io.s, io.mCLINT)
+      when(io.s.bready && io.mCLINT.bvalid) {
+        wstate := State.sIdle
+      }
+    }
+    is(State.sRAM) {
+      connectW(io.s, io.mRAM)
+      when(io.s.bready && io.mRAM.bvalid) {
+        wstate := State.sIdle
+      }
+    }
+  }
+
+  val rstate = RegInit(State.sIdle)
+  switch(rstate) {
+    is(State.sIdle) {
+      when(io.s.arvalid) {
+        when(UART.contains(io.s.araddr)) {
+          rstate := State.sUART
+        }.elsewhen(CLINT.contains(io.s.araddr)) {
+          rstate := State.sCLINT
+        }.otherwise {
+          rstate := State.sRAM
+        }
+      }
+    }
+    is(State.sUART) {
+      connectR(io.s, io.mUART)
+      when(io.s.rready && io.mUART.rvalid && io.mUART.rlast) {
+        rstate := State.sIdle
+      }
+    }
+    is(State.sCLINT) {
+      connectR(io.s, io.mCLINT)
+      when(io.s.rready && io.mCLINT.rvalid && io.mCLINT.rlast) {
+        rstate := State.sIdle
+      }
+    }
+    is(State.sRAM) {
+      connectR(io.s, io.mRAM)
+      when(io.s.rready && io.mRAM.rvalid && io.mRAM.rlast) {
+        rstate := State.sIdle
       }
     }
   }
