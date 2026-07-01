a9b818246219b23d56c9e05fa2fa592970881661
tracer-ysyx <tracer@ysyx.org>
2026-07-01 21:44:28 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:44:28 up 2 days, 45 min,  1 user,  load average: 0.38, 0.49, 0.49

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -14,6 +14,7 @@ Context *__am_irq_handle(Context *c)
     {
       case 11: ev.event = EVENT_YIELD;c->mepc += 4; break;
     default:
+      c->mepc += 4;
       ev.event = EVENT_ERROR;
       break;
     }
