c3a02ab95233d552294a2865c2f5897562f8e646
tracer-ysyx <tracer@ysyx.org>
2026-07-04 23:21:57 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:21:57 up 19:17,  1 user,  load average: 1.49, 1.22, 0.80

--- npc/src/main/scala/core/LSU.scala
+++ npc/src/main/scala/core/LSU.scala
@@ -2,19 +2,8 @@ package top
 
 import chisel3._
 import chisel3.util._
-class LSU2WBU extends Bundle {
+class LSU2WBU extends EXU2LSU {
   val memRdata = UInt(32.W)
-  val ctrl     = new CtrlBundle
-  val result   = UInt(32.W)
-  val pc       = UInt(32.W)
-  val pc4 = UInt(32.W)
-  val imm      = UInt(32.W)
-  val rd       = UInt(5.W)
-  val rdata1   = UInt(32.W)
-  val csrRdata = UInt(32.W)
-  val npc      = UInt(32.W)
-  val inst     = UInt(32.W)
-  val pfm_tag  = UInt(8.W)
 }
 class LSU     extends Module {
   val io = IO(new Bundle {
