e64f9533028f0dbf773d9b1f2fb0ef78a4a05df9
tracer-ysyx <tracer@ysyx.org>
2026-07-08 17:38:19 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:38:19 up 4 days,  4:21,  1 user,  load average: 0.54, 0.81, 0.70

--- npc/src/main/scala/top.scala
+++ npc/src/main/scala/top.scala
@@ -161,7 +161,7 @@ class ysyx_26010036 extends Module {
 
     dpic.io.idu_raw := (idu.io.raw.rs1RAW && !idu.io.raw.rs1fwdValid) || (idu.io.raw.rs2RAW && !idu.io.raw.rs2fwdValid) || idu.io.raw.csrRAW
 
-    dpic.io.branch_correct := exu.io.in.valid && exu.io.in.bits.ctrl.pcSel === PcSel.BRANCH && !exu.io.redirectEn
+    dpic.io.branch_correct := exu.io.out.fire && exu.io.in.bits.ctrl.pcSel === PcSel.BRANCH && !exu.io.redirectEn
 
     dpic.io.lsu_r_begin  := lsu.io.axi.arvalid && lsu.io.axi.arready
     dpic.io.lsu_r_finish := lsu.io.axi.rvalid && lsu.io.axi.rready && lsu.io.axi.rlast
