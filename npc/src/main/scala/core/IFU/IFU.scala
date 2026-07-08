f055f84783e37c5c74a2e839826e08c261fe0c45
tracer-ysyx <tracer@ysyx.org>
2026-07-08 15:31:44 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:31:44 up 4 days,  2:15,  1 user,  load average: 0.43, 0.39, 0.35

--- npc/src/main/scala/core/IFU/IFU.scala
+++ npc/src/main/scala/core/IFU/IFU.scala
@@ -16,7 +16,7 @@ class BTBEntry extends Bundle {
 }
 
 class IFU extends Module {
-  val io  = IO(new Bundle {
+  val io          = IO(new Bundle {
     val out          = Decoupled(new IFU2ICA)
     val redirectEn   = Input(Bool())
     val redirectPc   = Input(UInt(32.W))
@@ -25,12 +25,17 @@ class IFU extends Module {
     val isBranch     = Input(Bool())
 
   })
-  val pc  = RegInit("h30000000".U(32.W))
+  val pc          = RegInit("h30000000".U(32.W))
   // val pc          = RegInit("h80000000".U(32.W))
-  val pc4 = WireInit((pc + 4.U)(31, 0))
+  val pc4         = WireInit((pc + 4.U)(31, 0))
+  val dpic_tagReg = RegInit(0.U(8.W))
+
+  io.out.bits.pc  := pc
+  io.out.bits.pc4 := pc4
+  io.out.bits.dpic_tag       := dpic_tagReg
+  io.out.valid    := false.B
 
   // 保存跳转信息
-  val dpic_tagReg     = RegInit(0.U(8.W))
   val pcOfBranchReg   = RegEnable(io.pcOfBranch, io.redirectEn)
   val branchOffsetReg = RegEnable(io.branchOffset, io.redirectEn)
   val isBranchReg     = RegEnable(io.isBranch, io.redirectEn)
@@ -61,6 +66,7 @@ class IFU extends Module {
   val updateBTB    = RegInit(false.B)
   val branchTaken  = WireInit(false.B)
   val branchNextPc = WireInit((pc + target.asSInt.pad(32).asUInt)(31, 0))
+  io.out.bits.branchPreTaken := branchTaken
   when(io.redirectEn) {
     pc          := io.redirectPc
     dpic_tagReg := dpic_tagReg + 1.U
@@ -103,11 +109,5 @@ class IFU extends Module {
 
     }
   }
-
-  io.out.bits.pc             := pc
-  io.out.bits.pc4            := pc4
-  io.out.bits.dpic_tag       := dpic_tagReg
-  io.out.bits.branchPreTaken := branchTaken
-  io.out.valid               := false.B
 
 }
