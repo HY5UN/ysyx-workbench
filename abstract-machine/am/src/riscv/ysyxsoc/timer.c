8b041fa891b8e73eb9e0edb00906e784b8ea9339
tracer-ysyx <tracer@ysyx.org>
2026-07-09 19:02:41 +0800

    > sim RTL ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:02:41 up  4:46,  1 user,  load average: 0.63, 0.91, 0.55

--- abstract-machine/am/src/riscv/ysyxsoc/timer.c
+++ abstract-machine/am/src/riscv/ysyxsoc/timer.c
@@ -5,8 +5,8 @@
 #include <stdint.h>
 
 // #define FREQ (1000 * 1000 * 1000)
-// #define FREQ (1150*1000)
-#define FREQ 1
+#define FREQ (1150*1000)
+// #define FREQ 1
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
