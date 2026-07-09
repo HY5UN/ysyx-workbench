9b6f025321bb3589e1074c324697ac77769339a6
tracer-ysyx <tracer@ysyx.org>
2026-07-09 22:31:21 +0800

    > generate verilog ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:31:21 up  8:15,  1 user,  load average: 0.66, 0.29, 0.24

--- npc/src/main/scala/top.scala
+++ npc/src/main/scala/top.scala
@@ -144,7 +144,7 @@ class ysyx_26010036 extends Module {
     dpic.io.if_bus_resp   := ica.io.axi.rvalid && ica.io.axi.rready && ica.io.axi.rlast
     dpic.io.ifu_tag       := ifu.io.out.bits.dpic_tag
 
-    dpic.io.idu_raw := (idu.io.raw.rs1RAW && !idu.io.raw.rs1fwdValid) || (idu.io.raw.rs2RAW && !idu.io.raw.rs2fwdValid) || idu.io.raw.csrRAW
+    dpic.io.idu_raw := idu.io.raw.stall
 
     dpic.io.branch_correct := exu.io.out.fire && exu.io.dpic_branchCorrect
 
