a14304e37aa130e1b7529ed4fa57fc7e1155f749
tracer-ysyx <tracer@ysyx.org>
2026-07-11 15:35:20 +0800

    > sim RTL - NPC ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:35:20 up 1 day,  3:49,  1 user,  load average: 0.34, 0.62, 0.45

--- npc/csrc/utils/npc_putch.cpp
+++ npc/csrc/utils/npc_putch.cpp
@@ -5,7 +5,7 @@ void dpic_putch(char c)
     putchar(c);
     if(c == '\n')
     {
-        printf ("[cycle: %llu]     ", (unsigned long long)cpu->cycle_count);
+        // printf ("[cycle: %llu]     ", (unsigned long long)cpu->cycle_count);
     }
 
 }
