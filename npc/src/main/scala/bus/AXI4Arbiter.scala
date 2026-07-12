d6a9e9b9c6353b169985e436813ee35a90c5ce58
tracer-ysyx <tracer@ysyx.org>
2026-07-12 15:02:25 +0800

    > sim RTL - ysyxSoC ysyx_22040000 孙浩益 Linux LAPTOP-46P11TE0 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:02:25 up 1 day, 16:11,  1 user,  load average: 1.78, 0.49, 0.16

--- npc/src/main/scala/bus/AXI4Arbiter.scala
+++ npc/src/main/scala/bus/AXI4Arbiter.scala
@@ -4,47 +4,65 @@ import chisel3._
 import chisel3.util._
 
 class AXI4Arbiter extends Module {
-  val io = IO(new Bundle {
+  val io    = IO(new Bundle {
     val sIFU = Flipped(new AXI4IO)
     val sLSU = Flipped(new AXI4IO)
     val m    = new AXI4IO
   })
-  DriveZeroSinks(io)
+  DriveZeroSinks(io) 
 
+  object State extends ChiselEnum {
+    val sIFU, sLSU = Value
+  }
+  val state = RegInit(State.sIFU)
+
+  val sIFU_Finish = RegInit(false.B)
+  val sLSU_Finish = RegInit(false.B)
+
+  // 读通道仲裁
+  switch(state) {
+    is(State.sIFU) {
+      io.sIFU.ar <> io.m.ar
+      io.sIFU.r  <> io.m.r
+
+      when(io.sIFU.ar.valid){
+        sIFU_Finish := false.B
+      }
+      when(io.sIFU.r.ready && io.m.r.valid && io.m.r.last){
+        sIFU_Finish := true.B
+      }
+      when(sIFU_Finish ){
+        when(io.sLSU.ar.valid){
+          io.m.ar.valid := false.B
+          io.sIFU.ar.ready := false.B
+          state := State.sLSU
+          sIFU_Finish := false.B
+        }
+      }
+    }
+    is(State.sLSU) {
+      io.sLSU.ar <> io.m.ar
+      io.sLSU.r  <> io.m.r
+
+      when(io.sLSU.ar.valid){
+        sLSU_Finish := false.B
+      }
+      when(io.sLSU.r.ready && io.m.r.valid && io.m.r.last){
+        sLSU_Finish := true.B
+      }
+      when(sLSU_Finish && !io.sLSU.ar.valid){
+        when(io.sIFU.ar.valid){
+          io.m.ar.valid := false.B
+          io.sLSU.ar.ready := false.B
+          state := State.sIFU
+          sLSU_Finish := false.B
+        }
+      }
+    }
+  }
+
+  // 写通道只有LSU访问
   io.m.aw <> io.sLSU.aw
   io.m.w  <> io.sLSU.w
   io.m.b  <> io.sLSU.b
-
-  
-  val outstandingQueue = Module(new Queue(Bool(), entries = 4))
-
-  val lsu_ar_req = io.sLSU.ar.valid
-  val ifu_ar_req = io.sIFU.ar.valid
-  val ar_allow   = outstandingQueue.io.enq.ready // FIFO 有空位才允许发起新请求
-
-  
-  io.m.ar.valid := (lsu_ar_req || ifu_ar_req) && ar_allow
-  io.m.ar.addr  := Mux(lsu_ar_req, io.sLSU.ar.addr, io.sIFU.ar.addr)
-
-  io.sLSU.ar.ready := io.m.ar.ready && ar_allow
-  io.sIFU.ar.ready := io.m.ar.ready && !lsu_ar_req && ar_allow // LSU 优先，IFU 需等待 LSU 闲置
-
-  val ar_fire = io.m.ar.valid && io.m.ar.ready
-  outstandingQueue.io.enq.valid := ar_fire
-  outstandingQueue.io.enq.bits  := lsu_ar_req
-
-  
-  val r_target_is_lsu = outstandingQueue.io.deq.bits
-  val r_valid_real    = io.m.r.valid && outstandingQueue.io.deq.valid
-
-  io.sLSU.r.valid := r_valid_real && r_target_is_lsu
-  io.sLSU.r.data  := io.m.r.data
-
-  io.sIFU.r.valid := r_valid_real && !r_target_is_lsu
-  io.sIFU.r.data  := io.m.r.data
-
-  io.m.r.ready := Mux(r_target_is_lsu, io.sLSU.r.ready, io.sIFU.r.ready)
-
-  val r_fire = io.m.r.valid && io.m.r.ready
-  outstandingQueue.io.deq.ready := r_fire && io.m.r.last
 }
