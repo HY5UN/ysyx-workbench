722d0da84c06fe6432d0485642d4951171edb7d9
H <superdb3401@gmail.com>
2026-08-08 15:08:10 +1000

    实现ndl timer

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <NDL.h>
 
 int main() {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  long long last = (long long)tv.tv_sec * 1000000 + tv.tv_usec;
-  printf("Timer start: %d.%06d s\n", (int)(last / 1000000), (int)(last % 1000000));
+  NDL_Init(0);
+  uint32_t last = NDL_GetTicks();
+  printf("Timer start: %u ms\n", (unsigned)last);
   while (1) {
-    gettimeofday(&tv, NULL);
-    long long now = (long long)tv.tv_sec * 1000000 + tv.tv_usec;
-    if (now - last >= 500000) {
-      printf("Timer: %d.%06d s\n", (int)(now / 1000000), (int)(now % 1000000));
+    uint32_t now = NDL_GetTicks();
+    if (now - last >= 500) {
+      printf("Timer: %u ms\n", (unsigned)now);
       last = now;
     }
   }
