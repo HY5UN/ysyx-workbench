889686fb5a9d3eb400da330834e16b5253d3c095
H <superdb3401@gmail.com>
2026-08-09 16:30:59 +1000

    启动bird

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,8 @@ void init_proc() {
   // naive_uload(NULL, "/bin/mytest");
   // naive_uload(NULL, "/bin/nslider");
   // naive_uload(NULL, "/bin/menu");
-  naive_uload(NULL, "/bin/nterm");
+  // naive_uload(NULL, "/bin/nterm");
+  naive_uload(NULL, "/bin/bird");
 
 }
 
--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,27 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  // open the file and get its size
+  FILE *fp = fopen(filename, "rb");
+  assert(fp != NULL);
+  fseek(fp, 0, SEEK_END);
+  int size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+
+  // allocate a buffer of size bytes and read the whole file into it
+  void *buf = malloc(size);
+  assert(buf != NULL);
+  size_t nread = fread(buf, 1, size, fp);
+  assert(nread == (size_t)size);
+
+  // decode the image from the buffer into an SDL_Surface
+  SDL_Surface *surf = STBIMG_LoadFromMemory(buf, size);
+
+  // close the file and free the buffer
+  fclose(fp);
+  free(buf);
+
+  return surf;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
